33bf4f4d1c41126e5795d449a24b5756a4fb644f
DIYgod <i@diygod.me>
2024-04-10 14:55:14 +0800

    feat: use react-router-dom

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,9 +3,23 @@ import './assets/main.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'debug',
+        lazy: () => import('./pages/debug')
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
