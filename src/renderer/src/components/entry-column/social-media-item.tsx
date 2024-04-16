9dc8be4f8410f897fc34819f01778f740aba2494
DIYgod <i@diygod.me>
2024-04-16 14:25:06 +0800

    feat: dynamic width for entry column

--- src/renderer/src/components/entry-column/social-media-item.tsx
+++ src/renderer/src/components/entry-column/social-media-item.tsx
@@ -12,7 +12,7 @@ export function SocialMediaItem({
         className="w-5 h-5 mr-2 rounded-sm shrink-0"
       />
       <div>
-        <div className="line-clamp-5 text-sm flex-1 -mt-0.5 leading-tight text-[13px]">
+        <div className="line-clamp-5 text-sm flex-1 -mt-0.5 text-[13px]">
           <div className='space-x-1'>
             <span className='font-medium'>{entry.author}</span>
             <span className="text-zinc-500">·</span>
--- src/renderer/src/pages/index.tsx
+++ src/renderer/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { FeedColumn } from '@renderer/components/feed-column'
 import { EntryColumn } from '@renderer/components/entry-column'
 import { useState } from 'react'
 import { ActivedList } from '@renderer/lib/types'
+import { cn } from '@renderer/lib/utils'
 
 export function Component() {
   const [activedList, setActivedList] = useState<ActivedList>(null)
@@ -12,7 +13,7 @@ export function Component() {
       <div className="w-64 pt-10 border-r shrink-0 flex flex-col bg-[#E1E0DF]" onClick={() => setActivedList(null)}>
         <FeedColumn activedList={activedList} setActivedList={setActivedList} />
       </div>
-      <div className="w-[340px] pt-10 border-r shrink-0 h-full overflow-y-auto">
+      <div className={cn("pt-10 border-r shrink-0 h-full overflow-y-auto", activedList?.type === "Social Media" ? "flex-1 min-w-96" : "w-[340px]")}>
         <EntryColumn activedList={activedList} activedEntry={activedEntry} setActivedEntry={setActivedEntry} />
       </div>
       <div className="flex-1 pt-10 px-4">Three</div>
