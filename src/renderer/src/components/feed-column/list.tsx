2fe373859e4d4b896534ba76cda6f51483c19045
DIYgod <i@diygod.me>
2024-05-18 11:44:44 +0800

    feat: category rename dialog

--- src/renderer/src/components/feed-column/list.tsx
+++ src/renderer/src/components/feed-column/list.tsx
@@ -22,6 +22,8 @@ import { showNativeMenu } from "@renderer/lib/native-menu"
 import { client } from "@renderer/lib/client"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { apiFetch } from "@renderer/lib/queries/api-fetch"
+import { CategoryRenameDialog } from "./category-rename-dialog"
+import { Dialog } from "../ui/dialog"
 
 export function FeedList({
   className,
@@ -92,14 +94,16 @@ function FeedCategory({
   expansion: boolean
 }) {
   const [open, setOpen] = useState(!data.name)
+  const [dialogOpen, setDialogOpen] = useState(false)
 
   const queryClient = useQueryClient()
+  const feedIdList = data.list.map((feed) => feed.feedId)
   const deleteMutation = useMutation({
     mutationFn: async () =>
       apiFetch("/categories", {
         method: "DELETE",
         body: {
-          feedIdList: data.list.map((feed) => feed.feedId),
+          feedIdList,
           deleteSubscriptions: false,
         },
       }),
@@ -143,63 +147,73 @@ function FeedCategory({
       onClick={(e) => e.stopPropagation()}
     >
       {!!data.name && (
-        <div
-          className={cn(
-            "flex items-center justify-between font-medium text-sm leading-loose px-2.5 py-[2px] rounded-md w-full transition-colors",
-            activedList?.level === levels.folder &&
-              activedList.name === data.name &&
-              "bg-[#C9C9C7]",
-          )}
-          onClick={(e) => {
-            e.stopPropagation()
-            setCatrgoryActive()
-          }}
-          onContextMenu={(e) => {
-            showNativeMenu(
-              [
-                {
-                  type: "text",
-                  label: "Rename Category",
-                  click: async () => {},
-                },
-                {
-                  type: "text",
-                  label: "Delete Category",
-                  click: async () => {
-                    if (
-                      await client.showConfirmDialog({
-                        title: `Delete Category ${data.name}?`,
-                        message: `This operation will delete your category, but the feeds it contains will be retained and grouped by website.`,
-                        options: {
-                          buttons: ["Delete", "Cancel"],
-                        },
-                      })
-                    ) {
-                      deleteMutation.mutate()
-                    }
+        <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+          <div
+            className={cn(
+              "flex items-center justify-between font-medium text-sm leading-loose px-2.5 py-[2px] rounded-md w-full transition-colors",
+              activedList?.level === levels.folder &&
+                activedList.name === data.name &&
+                "bg-[#C9C9C7]",
+            )}
+            onClick={(e) => {
+              e.stopPropagation()
+              setCatrgoryActive()
+            }}
+            onContextMenu={(e) => {
+              showNativeMenu(
+                [
+                  {
+                    type: "text",
+                    label: "Rename Category",
+                    click: () => setDialogOpen(true),
                   },
-                },
-              ],
-              e,
-            )
-          }}
-        >
-          <div className="flex items-center min-w-0 w-full">
-            <CollapsibleTrigger
-              className={cn(
-                "flex items-center h-7 [&_.i-mingcute-right-fill]:data-[state=open]:rotate-90",
-                !setActivedList && "flex-1",
-              )}
-            >
-              <i className="i-mingcute-right-fill mr-2 transition-transform" />
-              {!setActivedList && <span className="truncate">{data.name}</span>}
-            </CollapsibleTrigger>
-            {setActivedList && <span className="truncate">{data.name}</span>}
+                  {
+                    type: "text",
+                    label: "Delete Category",
+                    click: async () => {
+                      if (
+                        await client.showConfirmDialog({
+                          title: `Delete Category ${data.name}?`,
+                          message: `This operation will delete your category, but the feeds it contains will be retained and grouped by website.`,
+                          options: {
+                            buttons: ["Delete", "Cancel"],
+                          },
+                        })
+                      ) {
+                        deleteMutation.mutate()
+                      }
+                    },
+                  },
+                ],
+                e,
+              )
+            }}
+          >
+            <div className="flex items-center min-w-0 w-full">
+              <CollapsibleTrigger
+                className={cn(
+                  "flex items-center h-7 [&_.i-mingcute-right-fill]:data-[state=open]:rotate-90",
+                  !setActivedList && "flex-1",
+                )}
+              >
+                <i className="i-mingcute-right-fill mr-2 transition-transform" />
+                {!setActivedList && (
+                  <span className="truncate">{data.name}</span>
+                )}
+              </CollapsibleTrigger>
+              {setActivedList && <span className="truncate">{data.name}</span>}
+            </div>
+            {!!data.unread && (
+              <div className="text-xs text-zinc-500 ml-2">{data.unread}</div>
+            )}
+            <CategoryRenameDialog
+              feedIdList={feedIdList}
+              view={view}
+              category={data.name}
+              onSuccess={() => setDialogOpen(false)}
+            />
           </div>
-          {!!data.unread && (
-            <div className="text-xs text-zinc-500 ml-2">{data.unread}</div>
-          )}
-        </div>
+        </Dialog>
       )}
       <AnimatePresence>
         {open && (
