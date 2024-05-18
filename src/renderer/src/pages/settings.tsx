6937f2a87aa2f505c14055a25d0d982d327225b2
DIYgod <i@diygod.me>
2024-05-18 19:43:37 +0800

    feat: settings page title

--- src/renderer/src/pages/settings.tsx
+++ src/renderer/src/pages/settings.tsx
@@ -1,3 +1,10 @@
 export function Component() {
-  return <>Settings</>
+  return (
+    <div
+      className="h-10 absolute top-0 inset-x-0 border-b flex items-center pl-20 text-sm font-medium"
+      aria-hidden
+    >
+      Follow Settings
+    </div>
+  )
 }
