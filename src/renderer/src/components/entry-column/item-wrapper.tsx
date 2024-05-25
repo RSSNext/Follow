bf93f180f706e92c41829819760e729ae0c3a35f
DIYgod <i@diygod.me>
2024-05-25 23:38:44 +0800

    feat: mark as read when hovered

--- src/renderer/src/components/entry-column/item-wrapper.tsx
+++ src/renderer/src/components/entry-column/item-wrapper.tsx
@@ -1,8 +1,13 @@
 import { useEntryActions } from "@renderer/hooks/useEntryActions"
+import { useUpdateEntry } from "@renderer/hooks/useUpdateEntry"
 import { showNativeMenu } from "@renderer/lib/native-menu"
 import type { EntriesResponse } from "@renderer/lib/types"
 import { cn } from "@renderer/lib/utils"
+import { apiFetch } from "@renderer/queries/api-fetch"
 import { feedActions, useFeedStore } from "@renderer/store"
+import { useMutation } from "@tanstack/react-query"
+import { useHover } from "@use-gesture/react"
+import { useRef } from "react"
 
 export function EntryItemWrapper({
   entry,
@@ -21,6 +26,35 @@ export function EntryItemWrapper({
   const activeEntry = useFeedStore(
     (state) => state.activeEntry,
   )
+
+  const updateEntry = useUpdateEntry({
+    entryId: entry?.id,
+  })
+
+  const read = useMutation({
+    mutationFn: async () =>
+      apiFetch("/reads", {
+        method: "POST",
+        body: {
+          entryId: entry?.id,
+        },
+      }),
+    onSuccess: () => {
+      updateEntry({
+        read: true,
+      })
+    },
+  })
+
+  const itemRef = useRef<HTMLDivElement>(null)
+  useHover((state) => {
+    if (state.active && !entry.read) {
+      read.mutate()
+    }
+  }, {
+    target: itemRef,
+  })
+
   if (!entry?.url || view === undefined) return children
 
   return (
@@ -29,7 +63,9 @@ export function EntryItemWrapper({
       className={cn(
         "rounded-md transition-colors",
         activeEntry === entry.id && "bg-[#DEDDDC]",
+        entry.read && "text-gray-500 opacity-90",
       )}
+      ref={itemRef}
       onClick={(e) => {
         e.stopPropagation()
         feedActions.setActiveEntry(entry.id)
