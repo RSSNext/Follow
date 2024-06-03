f650ea5b257676bdf50c4e9e4ce93a2a479d34d9
Innei <i@innei.in>
2024-06-03 18:45:01 +0800

    chore: remove todo
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/components/appearance-switch.tsx
+++ src/renderer/src/components/appearance-switch.tsx
@@ -6,7 +6,6 @@ export function AppearanceSwitch() {
   const { isDark, toggleDark } = useDark()
   return (
     <div className="flex items-center justify-between gap-4">
-      {/* TODO system color mode */}
       <p>Dark Mode</p>
       <Switch checked={isDark} onCheckedChange={toggleDark} />
     </div>
