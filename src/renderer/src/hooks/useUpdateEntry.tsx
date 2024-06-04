c0b4c520eb0b3840323f5ce803e62d3721bad2a2
Innei <i@innei.in>
2024-06-04 16:32:45 +0800

    chore: comment deprecated code
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/hooks/useUpdateEntry.tsx
+++ src/renderer/src/hooks/useUpdateEntry.tsx
@@ -1,73 +1,74 @@
-import type { EntriesResponse, ListResponse } from "@renderer/models"
-import { Queries } from "@renderer/queries"
-import type { Response as SubscriptionsResponse } from "@renderer/queries/subscriptions"
-import type { InfiniteData, QueryKey } from "@tanstack/react-query"
-import { useQueryClient } from "@tanstack/react-query"
-import { produce } from "immer"
+// import type { EntriesResponse, FeedListModel, ListResponse, SubscriptionModel } from "@renderer/models"
+// import { Queries } from "@renderer/queries"
+// import type { InfiniteData, QueryKey } from "@tanstack/react-query"
+// import { useQueryClient } from "@tanstack/react-query"
+// import { produce } from "immer"
 
 /**
  *
  * @deprecated
  */
-export const useUpdateEntry = ({
-  entryId,
-  feedId,
-}: {
-  entryId?: string
-  feedId?: string
-}) => {
-  const queryClient = useQueryClient()
 
-  const updateEntry = (changed: Partial<EntriesResponse[number]>) => {
-    const query = Queries.entries.byId(entryId)
+// export const useUpdateEntry = ({
+//   entryId,
+//   feedId,
+// }: {
+//   entryId?: string
+//   feedId?: string
+// }) => {
+//   const queryClient = useQueryClient()
 
-    query.setData((draft) => {
-      if (!draft) return
-      Object.assign(draft, changed)
-    })
+//   const updateEntry = (changed: Partial<EntriesResponse[number]>) => {
+//     const query = Queries.entries.byId(entryId)
 
-    const entriesData = queryClient.getQueriesData({
-      queryKey: ["entries"],
-    })
-    entriesData.forEach(([key, data]: [QueryKey, unknown]) => {
-      const assertData = data as InfiniteData<ListResponse<EntriesResponse>>
-      const finaldata = produce(assertData, (assertData) => {
-        const list = assertData?.pages?.[0]?.data
-        if (list) {
-          for (const item of list) {
-            if (item.entries.id === entryId) {
-              for (const [key, value] of Object.entries(changed)) {
-                item[key] = value
-              }
-            }
-          }
-        }
-      })
-      queryClient.setQueryData<typeof assertData>(key, finaldata)
-    })
+//     query.setData((draft) => {
+//       if (!draft) return
+//       Object.assign(draft, changed)
+//     })
 
-    if (changed.read !== undefined) {
-      const entriesData = queryClient.getQueriesData({
-        queryKey: ["subscriptions"],
-      })
-      entriesData.forEach(([key, data]: [QueryKey, unknown]) => {
-        const change = changed.read ? -1 : 1
-        const assertData = data as SubscriptionsResponse
-        const finaldata = produce(assertData, (assertData) => {
-          for (const list of assertData.list) {
-            for (const item of list.list) {
-              if (item.feeds.id === feedId) {
-                assertData.unread += change
-                list.unread += change
-                item.unread = (item.unread || 0) + change
-              }
-            }
-          }
-        })
-        queryClient.setQueryData<typeof assertData>(key, finaldata)
-      })
-    }
-  }
+//     const entriesData = queryClient.getQueriesData({
+//       queryKey: ["entries"],
+//     })
+//     entriesData.forEach(([key, data]: [QueryKey, unknown]) => {
+//       const assertData = data as InfiniteData<ListResponse<EntriesResponse>>
+//       const finaldata = produce(assertData, (assertData) => {
+//         const list = assertData?.pages?.[0]?.data
+//         if (list) {
+//           for (const item of list) {
+//             if (item.entries.id === entryId) {
+//               for (const [key, value] of Object.entries(changed)) {
+//                 item[key] = value
+//               }
+//             }
+//           }
+//         }
+//       })
+//       queryClient.setQueryData<typeof assertData>(key, finaldata)
+//     })
 
-  return updateEntry
-}
+//     if (changed.read !== undefined) {
+//       const entriesData = queryClient.getQueriesData({
+//         queryKey: ["subscriptions"],
+//       })
+//       entriesData.forEach(([key, data]: [QueryKey, unknown]) => {
+//         const change = changed.read ? -1 : 1
+//         const assertData = data as FeedListModel
+//         const finaldata = produce(assertData, (assertData) => {
+//           for (const list of assertData.list) {
+//             for (const item of list.list) {
+//               if (item.feeds.id === feedId) {
+//                 assertData.unread += change
+//                 list.unread += change
+//                 item.unread = (item.unread || 0) + change
+//               }
+//             }
+//           }
+//         })
+//         queryClient.setQueryData<typeof assertData>(key, finaldata)
+//       })
+//     }
+//   }
+
+//   return updateEntry
+// }
+export {}
