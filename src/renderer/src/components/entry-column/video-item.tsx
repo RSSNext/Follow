2abf5379113562f1b0311a1d6cfe7101842886f4
DIYgod <i@diygod.me>
2024-06-05 18:04:16 +0800

    feat: video preview for video items

--- src/renderer/src/components/entry-column/video-item.tsx
+++ src/renderer/src/components/entry-column/video-item.tsx
@@ -1,30 +1,54 @@
 import { FeedIcon } from "@renderer/components/feed-icon"
 import { Image } from "@renderer/components/ui/image"
 import dayjs from "@renderer/lib/dayjs"
+import { urlToIframe } from "@renderer/lib/url-to-iframe"
 import { cn } from "@renderer/lib/utils"
 import { useEntry } from "@renderer/store/entry"
+import { useHover } from "@use-gesture/react"
+import { useMemo, useRef, useState } from "react"
 
 import { ReactVirtuosoItemPlaceholder } from "../ui/placeholder"
 import type { UniversalItemProps } from "./types"
 
 export function VideoItem({ entryId, entryPreview }: UniversalItemProps) {
   const entry = useEntry(entryId) || entryPreview
+
+  const iframeSrc = useMemo(() => urlToIframe(entry.entries.url), [entry.entries.url])
+
+  const ref = useRef<HTMLDivElement>(null)
+  const [hovered, setHovered] = useState(false)
+  useHover(
+    (event) => {
+      setHovered(event.active)
+    },
+    {
+      target: ref,
+    },
+  )
+
   if (!entry) return <ReactVirtuosoItemPlaceholder />
   return (
     <div className="flex">
       <div className="w-full">
-        <div className="flex gap-2 overflow-x-auto">
-          <Image
-            key={entry.entries.images?.[0]}
-            src={entry.entries.images?.[0]}
-            className="aspect-video w-full shrink-0 rounded-md object-cover"
-            loading="lazy"
-            proxy={{
-              width: 640,
-              height: 360,
-            }}
-            disableContextMenu
-          />
+        <div className="overflow-x-auto" ref={ref}>
+          {iframeSrc && hovered ? (
+            <iframe
+              src={iframeSrc}
+              className="pointer-events-none aspect-video w-full shrink-0 rounded-md bg-black object-cover"
+            />
+          ) : (
+            <Image
+              key={entry.entries.images?.[0]}
+              src={entry.entries.images?.[0]}
+              className="aspect-video w-full shrink-0 rounded-md object-cover"
+              loading="lazy"
+              proxy={{
+                width: 640,
+                height: 360,
+              }}
+              disableContextMenu
+            />
+          )}
         </div>
         <div className="flex-1 px-2 pb-3 pt-1 text-sm">
           <div className={cn("relative mb-0.5 mt-1 truncate font-medium", !!entry.collections && "pr-4")}>
---
+++ src/renderer/src/lib/url-to-iframe.ts
@@ -0,0 +1,20 @@
+export const urlToIframe = (url?: string | null) => {
+  if (url?.match(/\/\/www.bilibili.com\/video\/BV\w+/)) {
+    return `https://player.bilibili.com/player.html?${new URLSearchParams({
+      isOutside: "true",
+      autoplay: "true",
+      danmaku: "true",
+      muted: "true",
+      highQuality: "true",
+      bvid: url.match(/\/\/www.bilibili.com\/video\/(BV\w+)/)?.[1] || "",
+    }).toString()}`
+  } else if (url?.match(/\/\/www.youtube.com\/watch\?v=\w+/)) {
+    return `https://www.youtube-nocookie.com/embed/${url.match(/\/\/www.youtube.com\/watch\?v=(\w+)/)?.[1]}?${new URLSearchParams({
+      controls: "0",
+      autoplay: "1",
+      muted: "1",
+    }).toString()}`
+  } else {
+    return null
+  }
+}
