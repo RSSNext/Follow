9734b72de9ce68ce9ab17d06dcaf5c6fd2e18703
Innei <i@innei.in>
2024-06-11 16:12:41 +0800

    refactor: gen route object (#51)
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/router.tsx
+++ src/renderer/src/router.tsx
@@ -1,152 +1,17 @@
-import { createBrowserRouter, Outlet } from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom"
 
 import App from "./App"
+import { convertGlobToRoutes } from "./lib/route-builder"
+
+const globTree = import.meta.glob("./pages/**/*.{tsx,jsx}")
+const tree = convertGlobToRoutes(globTree)
+// console.log(tree)
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "",
-        lazy: () => import("./pages/(main)/layout"),
-        children: [
-          {
-            path: "",
-            lazy: () => import("./pages/(main)/(context)/layout"),
-            children: [
-              {
-                path: "",
-                lazy: () => import("./pages/(main)/(context)/index"),
-              },
 
-              {
-                path: "",
-                Component: Outlet,
-                children: [
-                  {
-                    path: "",
-                    lazy: () =>
-                      import("./pages/(main)/(context)/(nonhome)/layout"),
-                    children: [
-                      {
-                        path: "/discover",
-                        lazy: () =>
-                          import(
-                            "./pages/(main)/(context)/(nonhome)/discover/layout"
-                          ),
-                        children: [
-                          {
-                            path: "",
-                            lazy: () =>
-                              import(
-                                "./pages/(main)/(context)/(nonhome)/discover/index"
-                              ),
-                          },
-                        ],
-                      },
-                      {
-                        path: "/profile",
-                        lazy: () =>
-                          import(
-                            "./pages/(main)/(context)/(nonhome)/profile/layout"
-                          ),
-                        children: [
-                          {
-                            path: "",
-                            lazy: () =>
-                              import(
-                                "./pages/(main)/(context)/(nonhome)/profile/index"
-                              ),
-                          },
-                        ],
-                      },
-
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: "",
-        lazy: () => import("./pages/(external)/layout"),
-        children: [
-          {
-            path: "login",
-            lazy: () => import("./pages/(external)/login"),
-          },
-          {
-            path: "redirect",
-            lazy: () => import("./pages/(external)/redirect"),
-          },
-          {
-            path: "debug",
-            lazy: () => import("./pages/(external)/debug"),
-          },
-          {
-            path: "",
-            lazy: () => import("./pages/(external)/(with-layout)/layout"),
-            children: [
-              {
-                path: "/feed/:id",
-                lazy: () =>
-                  import("./pages/(external)/(with-layout)/feed/layout"),
-                children: [
-                  {
-                    path: "",
-                    lazy: () =>
-                      import("./pages/(external)/(with-layout)/feed/index"),
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: "settings",
-        lazy: () => import("./pages/settings/layout"),
-        children: [
-          {
-            path: "",
-            lazy: () => import("./pages/settings/index"),
-          },
-          {
-            path: "actions",
-            lazy: () => import("./pages/settings/actions"),
-          },
-          {
-            path: "rsshub",
-            lazy: () => import("./pages/settings/rsshub"),
-          },
-          {
-            path: "profile",
-            lazy: () => import("./pages/settings/profile"),
-          },
-        ],
-      },
-
-      {
-        path: "preview",
-        children: [
-          {
-            path: "",
-            lazy: () => import("./pages/preview"),
-          },
-        ],
-      },
-      {
-        path: "add",
-        children: [
-          {
-            path: "",
-            lazy: () => import("./pages/add/index"),
-          },
-        ],
-      },
-    ],
+    children: tree,
   },
 ])
