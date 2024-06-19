5b1b1d88fce9fdc021f627a25eeedeb6613d04dd
Innei <i@innei.in>
2024-06-19 19:37:08 +0800

    fix: cleanup
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/store/utils/local.ts
+++ src/renderer/src/store/utils/local.ts
@@ -1,11 +1,12 @@
 import { entryActions } from "../entry/store"
+import { feedActions } from "../feed"
 import { subscriptionActions } from "../subscription"
 import { uiActions } from "../ui"
 import { unreadActions } from "../unread"
 
 export const clearLocalPersistStoreData = () => {
   // All clear and reset method will aggregate here
-  [entryActions, subscriptionActions, unreadActions, uiActions].forEach(
+  [entryActions, subscriptionActions, unreadActions, uiActions, feedActions].forEach(
     (actions) => {
       actions.clear()
     },
