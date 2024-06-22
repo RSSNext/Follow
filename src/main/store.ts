9ae1794b91919e85c1308e353e91cea8f3fe2403
Innei <i@innei.in>
2024-06-22 13:35:30 +0800

    chore: update deps
    Signed-off-by: Innei <i@innei.in>

--- src/main/store.ts
+++ src/main/store.ts
@@ -1,3 +1,8 @@
 import Store from "electron-store"
 
-export const store = new Store()
+interface StoreType {
+  set: (key: string, value: any) => void
+  get: (key: string) => any
+}
+// https://github.com/sindresorhus/electron-store/issues/276
+export const store = new Store() as unknown as StoreType
