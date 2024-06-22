02e6b5182956124a4753338973991cf5fc3f6aeb
Innei <i@innei.in>
2024-06-22 15:08:57 +0800

    fix: launch at login only in electron (#82)
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/pages/settings/index.tsx
+++ src/renderer/src/pages/settings/index.tsx
@@ -4,7 +4,6 @@ import { useDark } from "@renderer/hooks"
 import { tipcClient } from "@renderer/lib/client"
 import { SettingsTitle } from "@renderer/modules/settings/title"
 import { useCallback, useEffect, useState } from "react"
-import { toast } from "sonner"
 
 export function Component() {
   const { isDark, toggleDark } = useDark()
@@ -19,28 +18,32 @@ export function Component() {
   const saveLoginSetting = useCallback((checked: boolean) => {
     tipcClient?.setLoginItemSettings(checked)
     setLoginSetting(checked)
-    toast.success("Settings saved.")
   }, [])
 
   const saveDarkSetting = useCallback(() => {
     toggleDark()
-    toast.success("Settings saved.")
   }, [])
 
   return (
     <>
       <SettingsTitle path="" sticky className="mb-4" />
       <div className="mt-6 space-y-6">
+        {window.electron && (
+          <div className="flex items-center gap-4">
+            <Checkbox
+              id="launch"
+              checked={loginSetting}
+              onCheckedChange={saveLoginSetting}
+            />
+            <Label htmlFor="launch">Launch Follow at Login</Label>
+          </div>
+        )}
         <div className="flex items-center gap-4">
           <Checkbox
-            id="launch"
-            checked={loginSetting}
-            onCheckedChange={saveLoginSetting}
+            id="dark"
+            checked={isDark}
+            onCheckedChange={saveDarkSetting}
           />
-          <Label htmlFor="launch">Launch Follow at Login</Label>
-        </div>
-        <div className="flex items-center gap-4">
-          <Checkbox id="dark" checked={isDark} onCheckedChange={saveDarkSetting} />
           <Label htmlFor="dark">Dark Mode</Label>
         </div>
       </div>
