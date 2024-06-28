e1ae88f6912ffeda7bd336e9204b4c3c087177da
Innei <i@innei.in>
2024-06-28 19:45:55 +0800

    fix: switch style
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/components/ui/switch.tsx
+++ src/renderer/src/components/ui/switch.tsx
@@ -8,7 +8,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-5 w-9 shrink-0 items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-theme-accent focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
       "duration-200 data-[state=checked]:bg-theme-accent data-[state=unchecked]:bg-theme-inactive dark:data-[state=unchecked]:bg-theme-accent-100/10",
       className,
     )}
@@ -16,10 +16,7 @@ const Switch = React.forwardRef<
     ref={ref}
   >
     <SwitchPrimitives.Thumb
-      className={cn(
-        "pointer-events-none block size-4 rounded-full ring-0 !transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
-        "bg-theme-background dark:bg-neutral-300",
-      )}
+      className="pointer-events-none block size-4 rounded-full bg-background ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0"
     />
   </SwitchPrimitives.Root>
 ))
