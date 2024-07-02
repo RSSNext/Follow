125f18ad112ffaa8a7eaf65c0f4e7c5d751681a5
Innei <i@innei.in>
2024-07-02 22:26:13 +0800

    fix: protocol
    Signed-off-by: Innei <i@innei.in>

--- src/shared/src/constants.ts
+++ src/shared/src/constants.ts
@@ -1,4 +1,4 @@
-export const APP_PROTOCOL = import.meta.env.DEV ? "follow" : "follow"
+export const APP_PROTOCOL = import.meta.env.DEV ? "follow-DEV" : "follow"
 export const DEEPLINK_SCHEME = `${APP_PROTOCOL}://`
 
 // export const WEB_URL = import.meta.env.VITE_VERCEL_URL ?? import.meta.env.VITE_WEB_URL
