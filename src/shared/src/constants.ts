1b9fb68788c6bfb1fc0a37246aaafcf10a374bcd
Innei <i@innei.in>
2024-07-02 17:31:03 +0800

    fix: web url env
    Signed-off-by: Innei <i@innei.in>

--- src/shared/src/constants.ts
+++ src/shared/src/constants.ts
@@ -1,4 +1,5 @@
 export const APP_PROTOCOL = import.meta.env.DEV ? "follow" : "follow"
 export const DEEPLINK_SCHEME = `${APP_PROTOCOL}://`
 
-export const WEB_URL = import.meta.env.VITE_VERCEL_URL ?? import.meta.env.VITE_WEB_URL
+// export const WEB_URL = import.meta.env.VITE_VERCEL_URL ?? import.meta.env.VITE_WEB_URL
+export const WEB_URL = import.meta.env.VITE_WEB_URL
