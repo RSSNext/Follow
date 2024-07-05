c344439db3f63bda99ad1916868b253a80ce165d
Innei <i@innei.in>
2024-07-05 20:31:21 +0800

    chore: update input and textarea focus styles
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/components/ui/input/TextArea.tsx
+++ src/renderer/src/components/ui/input/TextArea.tsx
@@ -86,6 +86,7 @@ export const TextArea = forwardRef<
                 "overflow-auto px-3 py-4",
                 "!outline-none",
                 "text-neutral-900/80 dark:text-slate-100/80",
+                "focus:!bg-theme-accent/5",
                 roundedMap[rounded],
                 className,
               )}
