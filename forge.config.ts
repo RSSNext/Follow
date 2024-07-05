955ca769aadc6df0750b6b75615575907d0ff3c9
DIYgod <i@diygod.me>
2024-07-05 20:55:51 +0800

    feat: replace electron builder with electron forge

---
+++ forge.config.ts
@@ -0,0 +1,46 @@
+import { FuseV1Options, FuseVersion } from "@electron/fuses"
+import { MakerDeb } from "@electron-forge/maker-deb"
+import { MakerDMG } from "@electron-forge/maker-dmg"
+import { MakerRpm } from "@electron-forge/maker-rpm"
+import { MakerSquirrel } from "@electron-forge/maker-squirrel"
+import { FusesPlugin } from "@electron-forge/plugin-fuses"
+import type { ForgeConfig } from "@electron-forge/shared-types"
+
+const config: ForgeConfig = {
+  packagerConfig: {
+    appBundleId: "is.follow",
+    icon: "resources/icon",
+    ignore: [
+      /^\/src/,
+      /(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.ts)|(tsconfig.*)/,
+    ],
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    new MakerSquirrel({}),
+    // new MakerZIP({}, ["darwin"]),
+    new MakerRpm({}),
+    new MakerDeb({
+      options: {
+        icon: "resources/icon.png",
+      },
+    }),
+    new MakerDMG({}),
+  ],
+  plugins: [
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+}
+
+export default config
