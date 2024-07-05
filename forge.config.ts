1c9b492a47d5a02ed4fbaf89de9db6f32ee5e23c
DIYgod <i@diygod.me>
2024-07-05 21:53:54 +0800

    chore: ci

--- forge.config.ts
+++ forge.config.ts
@@ -3,6 +3,7 @@ import { MakerDeb } from "@electron-forge/maker-deb"
 import { MakerDMG } from "@electron-forge/maker-dmg"
 import { MakerRpm } from "@electron-forge/maker-rpm"
 import { MakerSquirrel } from "@electron-forge/maker-squirrel"
+import { MakerZIP } from "@electron-forge/maker-zip"
 import { FusesPlugin } from "@electron-forge/plugin-fuses"
 import type { ForgeConfig } from "@electron-forge/shared-types"
 
@@ -19,7 +20,7 @@ const config: ForgeConfig = {
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
-    // new MakerZIP({}, ["darwin"]),
+    new MakerZIP({}, ["darwin"]),
     new MakerRpm({}),
     new MakerDeb({
       options: {
