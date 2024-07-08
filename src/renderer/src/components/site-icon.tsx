ed04be028cb82e511f57bb1e93e7d774743ae5da
DIYgod <i@diygod.me>
2024-07-08 20:10:08 +0800

    feat: update unavatar link

--- src/renderer/src/components/site-icon.tsx
+++ src/renderer/src/components/site-icon.tsx
@@ -20,7 +20,7 @@ export function SiteIcon({
     host = new URL(url).host
     const pureDomain = parse(host).domainWithoutSuffix
     fallback = `https://avatar.vercel.sh/${pureDomain}.svg?text=${pureDomain?.slice(0, 2).toUpperCase()}`
-    src = `https://unavatar.io/${host}?fallback=${fallback}`
+    src = `https://unavatar.follow.is/${host}?fallback=${fallback}`
   } catch {
     const pureDomain = parse(url).domainWithoutSuffix
     src = `https://avatar.vercel.sh/${pureDomain}.svg?text=${pureDomain?.slice(0, 2).toUpperCase()}`
