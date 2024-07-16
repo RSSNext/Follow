0203b5bf2687e621029ff53a52d01e17505b6149
Innei <i@innei.in>
2024-07-17 00:16:46 +0800

    fix: ui
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/modules/entry-content/header.tsx
+++ src/renderer/src/modules/entry-content/header.tsx
@@ -53,37 +53,42 @@ export function EntryHeader({
   return (
     <div
       className={cn(
-        "flex min-w-0 items-center justify-between gap-3 overflow-hidden text-lg text-zinc-500",
+        "relative flex min-w-0 items-center justify-between gap-3 overflow-hidden text-lg text-zinc-500",
         entryTitleMeta && "border-b border-border",
         className,
       )}
     >
-      <div className="flex min-w-0 shrink">
-        <AnimatePresence>
-          {entryTitleMeta && (
-            <m.div
-              initial={{ opacity: 0.01, y: 30 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0.01, y: 30 }}
-              className="flex min-w-0 shrink items-end gap-2 truncate text-sm leading-tight text-theme-foreground"
-            >
-              <span className="min-w-0 shrink truncate font-bold">
-                {entryTitleMeta.title}
-              </span>
-              <i className="i-mgc-line-cute-re size-[10px] shrink-0 translate-y-[-3px] rotate-[-25deg]" />
-              <span className="shrink-0 truncate text-xs opacity-80">
-                {entryTitleMeta.description}
-              </span>
-            </m.div>
-          )}
-        </AnimatePresence>
+      <div
+        className={cn(
+          "absolute left-5 top-0 z-0 flex h-full items-center gap-2 text-[13px] text-zinc-500",
+          !entryTitleMeta && "z-[11]",
+        )}
+      >
+        <EntryReadHistory entryId={entryId} />
       </div>
-      <div className="flex w-full items-center justify-between">
-        <div className="flex items-center gap-2 text-[13px] text-zinc-500">
-          <EntryReadHistory entryId={entryId} />
+      <div className="relative z-10 flex w-full items-center justify-between gap-3 bg-background">
+        <div className="flex min-w-0 shrink">
+          <AnimatePresence>
+            {entryTitleMeta && (
+              <m.div
+                initial={{ opacity: 0.01, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0.01, y: 30 }}
+                className="flex min-w-0 shrink items-end gap-2 truncate text-sm leading-tight text-theme-foreground"
+              >
+                <span className="min-w-0 shrink truncate font-bold">
+                  {entryTitleMeta.title}
+                </span>
+                <i className="i-mgc-line-cute-re size-[10px] shrink-0 translate-y-[-3px] rotate-[-25deg]" />
+                <span className="shrink-0 truncate text-xs opacity-80">
+                  {entryTitleMeta.description}
+                </span>
+              </m.div>
+            )}
+          </AnimatePresence>
         </div>
 
-        <div className="flex shrink-0 items-center gap-3">
+        <div className="relative flex w-full items-center justify-end gap-3">
           {items
             .filter((item) => !item.disabled)
             .map((item) => (
@@ -101,6 +106,7 @@ export function EntryHeader({
                 key={item.name}
               />
             ))}
+
         </div>
       </div>
     </div>
