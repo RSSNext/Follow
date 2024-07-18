52d97253d184b00501a7f81b6f46d56bfc9ef15d
Innei <i@innei.in>
2024-07-18 19:50:10 +0800

    feat: update about desc
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/modules/settings/tabs/about.tsx
+++ src/renderer/src/modules/settings/tabs/about.tsx
@@ -1,13 +1,10 @@
-import { repository } from "@pkg"
+import { license, repository } from "@pkg"
 import { Logo } from "@renderer/components/icons/logo"
 import { StyledButton } from "@renderer/components/ui/button"
 
 export const SettingAbout = () => (
   <div>
-    <section>
-      <div className="my-4 font-medium text-theme-foreground/60">
-        Version
-      </div>
+    <section className="mt-4">
       <div className="flex gap-3">
         <Logo className="size-[52px]" />
 
@@ -35,6 +32,35 @@ export const SettingAbout = () => (
           </StyledButton>
         </div>
       </div>
+
+      <p className="mt-3 text-balance text-sm">
+        {APP_NAME}
+        {" "}
+        is a free and open-source project. It is licensed under the
+        {" "}
+        {license}
+        {" "}
+        License.
+      </p>
+
+      <p className="mt-3 text-sm">
+        {APP_NAME}
+        {" "}
+        (
+        {GIT_COMMIT_SHA.slice(0, 7).toUpperCase()}
+        )
+        {" "}
+        currently early in development. If you have any feedback or
+        suggestions, please feel free to
+        {" "}
+        <a className="inline-flex cursor-pointer items-center gap-1 hover:underline" href={`${repository.url}/issues/new`} target="_blank">
+          open an issue
+          {" "}
+          <i className="i-mgc-external-link-cute-re" />
+        </a>
+        {" "}
+        on the GitHub.
+      </p>
     </section>
   </div>
 )
