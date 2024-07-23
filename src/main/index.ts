108fd389fb4285ee5443f139d0600be620acc8c9
DIYgod <i@diygod.me>
2024-07-23 23:50:41 +0800

    fix: macOS quit

--- src/main/index.ts
+++ src/main/index.ts
@@ -130,16 +130,16 @@ function bootsharp() {
         resizable,
       })
     }
-
-    // Quit when all windows are closed, except on  macOS. There, it's common
-    // for applications and their menu bar to stay active until the user quits
-    // explicitly with Cmd + Q.
-    app.on("window-all-closed", () => {
-      if (process.platform !== "darwin") {
-        app.quit()
-      }
-    })
   }
+
+  // Quit when all windows are closed, except on  macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit()
+    }
+  })
 }
 
 bootsharp()
