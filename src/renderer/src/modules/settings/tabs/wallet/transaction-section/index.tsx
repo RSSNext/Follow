30be690b166b41fec03b540ca8687d33dee63866
Innei <i@innei.in>
2024-07-27 21:53:44 +0800

    fix: power system icon and name
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/modules/settings/tabs/wallet/transaction-section/index.tsx
+++ src/renderer/src/modules/settings/tabs/wallet/transaction-section/index.tsx
@@ -1,6 +1,18 @@
 import { useMe } from "@renderer/atoms/user"
-import { Avatar, AvatarFallback, AvatarImage } from "@renderer/components/ui/avatar"
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@renderer/components/ui/table"
+import { Logo } from "@renderer/components/icons/logo"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@renderer/components/ui/avatar"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@renderer/components/ui/table"
 import { Balance } from "@renderer/components/ui/wallet/balance"
 import { cn } from "@renderer/lib/utils"
 import { SettingSectionTitle } from "@renderer/modules/settings/section"
@@ -16,7 +28,7 @@ export const TransactionsSection = () => {
 
   if (!myWallet) return
 
-  if (transactions.isLoading) return <div className="text-theme-disabled">Loading...</div>
+  if (transactions.isLoading) { return <div className="text-theme-disabled">Loading...</div> }
 
   // if (transactions.data?.length === 0) return <div className="text-theme-disabled">No transactions</div>
 
@@ -28,21 +40,11 @@ export const TransactionsSection = () => {
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead size="sm">
-                Type
-              </TableHead>
-              <TableHead size="sm">
-                Amount
-              </TableHead>
-              <TableHead size="sm">
-                From
-              </TableHead>
-              <TableHead size="sm">
-                To
-              </TableHead>
-              <TableHead size="sm">
-                Date
-              </TableHead>
+              <TableHead size="sm">Type</TableHead>
+              <TableHead size="sm">Amount</TableHead>
+              <TableHead size="sm">From</TableHead>
+              <TableHead size="sm">To</TableHead>
+              <TableHead size="sm">Date</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
@@ -72,20 +74,29 @@ export const TransactionsSection = () => {
           </TableBody>
         </Table>
         {!transactions.data?.length && (
-          <div className="my-2 w-full text-center text-sm text-zinc-400">No transactions</div>
+          <div className="my-2 w-full text-center text-sm text-zinc-400">
+            No transactions
+          </div>
         )}
       </div>
     </div>
   )
 }
 
-const TypeRenderer = ({ type }: { type: NonNullable<ReturnType<typeof useWalletTransactions>["data"]>[number]["type"] }) => (
-  <div className={cn("center rounded-full p-px text-xs uppercase", {
-    "bg-theme-accent-700 text-white": type === "tip",
-    "bg-green-700 text-white": type === "mint",
-    "bg-red-700 text-white": type === "burn",
-    "bg-yellow-700 text-white": type === "withdraw",
-  })}
+const TypeRenderer = ({
+  type,
+}: {
+  type: NonNullable<
+    ReturnType<typeof useWalletTransactions>["data"]
+  >[number]["type"]
+}) => (
+  <div
+    className={cn("center rounded-full p-px text-xs uppercase", {
+      "bg-theme-accent-700 text-white": type === "tip",
+      "bg-green-700 text-white": type === "mint",
+      "bg-red-700 text-white": type === "burn",
+      "bg-yellow-700 text-white": type === "withdraw",
+    })}
   >
     {type}
   </div>
@@ -100,28 +111,39 @@ const BalanceRenderer = ({
     ReturnType<typeof useWalletTransactions>["data"]
   >[number]["powerToken"]
 }) => (
-  <div className={cn("flex items-center justify-center", {
-    "text-green-500": sign === "+",
-    "text-red-500": sign === "-",
-  })}
+  <div
+    className={cn("flex items-center justify-center", {
+      "text-green-500": sign === "+",
+      "text-red-500": sign === "-",
+    })}
   >
     {sign}
     <Balance>{amount}</Balance>
   </div>
 )
 
-const UserRenderer = ({ user }: { user?: NonNullable<ReturnType<typeof useWalletTransactions>["data"]>[number]["fromUser" | "toUser"] }) => {
+const UserRenderer = ({
+  user,
+}: {
+  user?: NonNullable<ReturnType<typeof useWalletTransactions>["data"]>[number][
+    | "fromUser"
+    | "toUser"]
+}) => {
   const me = useMe()
   const isMe = user?.id === me?.id
 
-  const name = isMe ? "You" : user?.name || "SYSTEM"
+  const name = isMe ? "You" : user?.name || APP_NAME
 
   return (
     <div className="center">
-      <Avatar className="aspect-square size-4">
-        <AvatarImage src={user?.image || undefined} />
-        <AvatarFallback>{name?.slice(0, 2)}</AvatarFallback>
-      </Avatar>
+      {name === APP_NAME ? (
+        <Logo className="aspect-square size-4" />
+      ) : (
+        <Avatar className="aspect-square size-4">
+          <AvatarImage src={user?.image || undefined} />
+          <AvatarFallback>{name?.slice(0, 2)}</AvatarFallback>
+        </Avatar>
+      )}
 
       <div className="ml-1">
         {isMe ? <span className="font-bold">You</span> : name}
