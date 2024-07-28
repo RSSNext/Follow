3cad84d3016deea8bf85b305ea7378ed1831e6a0
Innei <i@innei.in>
2024-07-28 11:16:05 +0800

    fix: update profile external page
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/pages/(external)/(with-layout)/profile/[id]/index.tsx
+++ src/renderer/src/pages/(external)/(with-layout)/profile/[id]/index.tsx
@@ -9,7 +9,7 @@ import { StyledButton } from "@renderer/components/ui/button"
 import { useAuthQuery, useTitle } from "@renderer/hooks/common"
 import { apiClient } from "@renderer/lib/api-fetch"
 import { defineQuery } from "@renderer/lib/defineQuery"
-import { capitalizeFirstLetter } from "@renderer/lib/utils"
+import { capitalizeFirstLetter, cn } from "@renderer/lib/utils"
 import { DEEPLINK_SCHEME } from "@shared/constants"
 import { useParams } from "react-router-dom"
 import { parse } from "tldts"
@@ -17,47 +17,52 @@ import { parse } from "tldts"
 export function Component() {
   const { id } = useParams()
 
-  const user = useAuthQuery(defineQuery(["profiles", id], async () => {
-    const res = await apiClient.profiles.$get({
-      query: { id: id! },
-    })
-    return res.data
-  }), {
-    enabled: !!id,
-  })
+  const user = useAuthQuery(
+    defineQuery(["profiles", id], async () => {
+      const res = await apiClient.profiles.$get({
+        query: { id: id! },
+      })
+      return res.data
+    }),
+    {
+      enabled: !!id,
+    },
+  )
 
-  const subscriptions = useAuthQuery(defineQuery(["subscriptions", user.data?.id], async () => {
-    const res = await apiClient.subscriptions.$get({
-      query: { userId: user.data?.id },
-    })
-    const groupFolder = {} as Record<string, typeof res.data>
+  const subscriptions = useAuthQuery(
+    defineQuery(["subscriptions", user.data?.id], async () => {
+      const res = await apiClient.subscriptions.$get({
+        query: { userId: user.data?.id },
+      })
+      const groupFolder = {} as Record<string, typeof res.data>
 
-    for (const subscription of (res.data || [])) {
-      if (!subscription.category && subscription.feeds) {
-        const { siteUrl } = subscription.feeds
-        if (!siteUrl) continue
-        const parsed = parse(siteUrl)
-        parsed.domain &&
-        (subscription.category = capitalizeFirstLetter(parsed.domain))
-      }
-      if (subscription.category) {
-        if (!groupFolder[subscription.category]) {
-          groupFolder[subscription.category] = []
+      for (const subscription of res.data || []) {
+        if (!subscription.category && subscription.feeds) {
+          const { siteUrl } = subscription.feeds
+          if (!siteUrl) continue
+          const parsed = parse(siteUrl)
+          parsed.domain &&
+          (subscription.category = capitalizeFirstLetter(parsed.domain))
         }
-        groupFolder[subscription.category].push(subscription)
+        if (subscription.category) {
+          if (!groupFolder[subscription.category]) {
+            groupFolder[subscription.category] = []
+          }
+          groupFolder[subscription.category].push(subscription)
+        }
       }
-    }
 
-    return groupFolder
-  }), {
-    enabled: !!user.data?.id,
-  })
+      return groupFolder
+    }),
+    {
+      enabled: !!user.data?.id,
+    },
+  )
   useTitle(user.data?.name)
   return (
     <>
       {user.data && (
-        <div className="mx-auto mt-12 flex max-w-5xl flex-col items-center justify-center p-4 lg:p-0">
-
+        <div className="container mx-auto mt-12 flex flex-col items-center justify-center p-4 lg:p-0">
           <Avatar className="aspect-square size-16">
             <AvatarImage src={user.data.image || undefined} />
             <AvatarFallback>{user.data.name?.slice(0, 2)}</AvatarFallback>
@@ -66,11 +71,9 @@ export function Component() {
             <div className="mb-2 mt-4 flex items-center text-2xl font-bold">
               <h1>{user.data.name}</h1>
             </div>
-            <div className="mb-8 text-sm text-zinc-500">
-              {user.data.handle}
-            </div>
+            <div className="mb-8 text-sm text-zinc-500">{user.data.handle}</div>
           </div>
-          <div className="mb-12 w-96 space-y-10">
+          <div className="mb-12 w-[70ch] max-w-full space-y-10">
             {Object.keys(subscriptions.data || {}).map((category) => (
               <div key={category}>
                 <div className="mb-4 flex items-center text-2xl font-bold">
@@ -78,19 +81,43 @@ export function Component() {
                 </div>
                 <div>
                   {subscriptions.data?.[category].map((subscription) => (
-                    <div key={subscription.feedId} className="group relative border-b py-5">
-                      <a className="flex flex-1 cursor-default" href={subscription.feeds.siteUrl!} target="_blank">
-                        <FeedIcon feed={subscription.feeds} size={22} className="mr-3" />
-                        <div className="flex-1">
-                          <div className="truncate font-medium leading-none">{subscription.feeds?.title}</div>
-                          <div className="mt-1 line-clamp-1 text-xs text-zinc-500">{subscription.feeds?.description}</div>
+                    <div
+                      key={subscription.feedId}
+                      className="group relative border-b py-5"
+                    >
+                      <a
+                        className="flex flex-1 cursor-default"
+                        href={subscription.feeds.siteUrl!}
+                        target="_blank"
+                      >
+                        <FeedIcon
+                          feed={subscription.feeds}
+                          size={22}
+                          className="mr-3"
+                        />
+                        <div
+                          className={cn(
+                            "w-0 flex-1 grow",
+                            "group-hover:grow-[0.85]",
+                          )}
+                        >
+                          <div className="truncate font-medium leading-none">
+                            {subscription.feeds?.title}
+                          </div>
+                          <div className="mt-1 line-clamp-1 text-xs text-zinc-500">
+                            {subscription.feeds?.description}
+                          </div>
                         </div>
-                      </a>
-                      <a className="absolute left-full top-5 opacity-0 transition-opacity group-hover:opacity-100" href={`${DEEPLINK_SCHEME}#add?id=${subscription.feeds?.id}`}>
-                        <StyledButton>
-                          <FollowIcon className="mr-1 size-3" />
-                          {APP_NAME}
-                        </StyledButton>
+
+                        <a
+                          className="absolute right-0 opacity-0 transition-opacity group-hover:opacity-100"
+                          href={`${DEEPLINK_SCHEME}#add?id=${subscription.feeds?.id}`}
+                        >
+                          <StyledButton>
+                            <FollowIcon className="mr-1 size-3" />
+                            {APP_NAME}
+                          </StyledButton>
+                        </a>
                       </a>
                     </div>
                   ))}
