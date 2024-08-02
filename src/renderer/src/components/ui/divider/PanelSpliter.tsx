60f8347eec118176681a9ad7fcab7f45e383fc1b
Innei <i@innei.in>
2024-08-02 13:02:33 +0800

    fix: type error
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/components/ui/divider/PanelSpliter.tsx
+++ src/renderer/src/components/ui/divider/PanelSpliter.tsx
@@ -2,14 +2,13 @@ import * as React from "react"
 
 export const PanelSplitter = (
   props: React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
   >,
 ) => (
   <div className="relative h-full w-0 shrink-0">
-    <button
+    <div
       tabIndex={-1}
-      type="button"
       {...props}
       className="absolute inset-0 z-[11] w-[2px] -translate-x-1/2 cursor-ew-resize bg-transparent hover:bg-gray-400 active:bg-theme-accent hover:dark:bg-neutral-500"
     />
