7678bd48607fa6cd94bbb6f11f8b03c815ac457e
Innei <i@innei.in>
2024-08-09 14:29:33 +0800

    chore: remove markdown-it, use remark
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/components/ui/markdown/Markdown.tsx
+++ src/renderer/src/components/ui/markdown/Markdown.tsx
@@ -1,27 +1,18 @@
+import { parseMarkdown } from "@renderer/lib/parse-markdown"
 import { cn } from "@renderer/lib/utils"
-import markdownit from "markdown-it"
 import { useMemo } from "react"
-
-import styles from "./index.module.css"
-import { createContainer } from "./plugins/container"
-
-const md = markdownit()
-  .use(...createContainer("tip", "TIP", () => md))
-  .use(...createContainer("info", "INFO", () => md))
-  .use(...createContainer("warning", "WARNING", () => md))
-  .use(...createContainer("danger", "DANGER", () => md))
-  .use(...createContainer("details", "Details", () => md))
 
 export const Markdown: Component<{
   children: string
-}> = (props) => (
-  <div
-    className={cn(styles.markdown, props.className, "prose dark:prose-invert prose-th:text-left")}
-    dangerouslySetInnerHTML={useMemo(
-      () => ({
-        __html: md.render(props.children as string),
-      }),
-      [props.children],
-    )}
-  />
-)
+}> = ({ children, className }) => {
+  const markdownElement = useMemo(
+    () => parseMarkdown(children).content,
+    [children],
+  )
+
+  return (
+    <article className={cn("prose relative cursor-auto select-text dark:prose-invert prose-th:text-left", className)}>
+      {markdownElement}
+    </article>
+  )
+}
