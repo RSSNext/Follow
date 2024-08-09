a888a789e10ecd8b3fab713e859bfe9f56bfe0ad
Innei <i@innei.in>
2024-08-09 22:59:50 +0800

    fix: add db index
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/store/image/db.ts
+++ src/renderer/src/store/image/db.ts
@@ -1,10 +1,27 @@
-import { createStore, get, set } from "idb-keyval"
+import type { UseStore } from "idb-keyval"
+import { get, promisifyRequest, set } from "idb-keyval"
 
 import type { StoreImageType } from "."
 
-const db = createStore("FOLLOW_IMAGE_DIMENSIONS", "image-dimensions")
+function createStore(dbName: string, storeName: string): UseStore {
+  const request = indexedDB.open(dbName)
+  request.onupgradeneeded = () => {
+    const objectStore = request.result.createObjectStore(storeName)
 
-export const getImageDimensionsFromDb = async (url: string) => await get(url, db)
+    objectStore.createIndex("src", "src", { unique: true })
+    return objectStore
+  }
+  const dbp = promisifyRequest(request)
+
+  return (txMode, callback) =>
+    dbp.then((db) =>
+      callback(db.transaction(storeName, txMode).objectStore(storeName)),
+    )
+}
+
+const db = createStore("FOLLOW_IMAGE_DIMENSIONS", "image-dimensions")
+export const getImageDimensionsFromDb = async (url: string) =>
+  await get(url, db)
 
 export const saveImageDimensionsToDb = async (
   url: string,
