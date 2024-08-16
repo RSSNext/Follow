8a8766f6d49fb347484a29852a5960bdc711e792
Innei <i@innei.in>
2024-08-16 21:49:57 +0800

    fix: test
    Signed-off-by: Innei <i@innei.in>

--- setup-file.ts
+++ setup-file.ts
@@ -6,9 +6,9 @@ import { enableMapSet } from "immer"
 globalThis.window = {
   location: new URL("https://example.com"),
   __dbIsReady: true,
-  navigator: {
-    onLine: true,
-  },
+}
 
+globalThis.navigator = {
+  onLine: true,
 }
 enableMapSet()
