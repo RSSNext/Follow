97779a0daab9ea488619a4fd0a10317a067b71dc
Innei <i@innei.in>
2024-08-22 20:37:54 +0800

    fix: type error
    Signed-off-by: Innei <i@innei.in>

--- src/shared/src/constants.ts
+++ src/shared/src/constants.ts
@@ -1,4 +1,12 @@
+import type { ElectronAPI } from "@electron-toolkit/preload"
 import { env } from "@env"
+
+declare const globalThis: {
+  window: Window & {
+    electron?: ElectronAPI
+    api?: { canWindowBlur: boolean }
+  }
+}
 
 export const APP_PROTOCOL = import.meta.env.DEV ? "follow-dev" : "follow"
 export const DEEPLINK_SCHEME = `${APP_PROTOCOL}://`
@@ -7,5 +15,5 @@ export const DEEPLINK_SCHEME = `${APP_PROTOCOL}://`
 export const WEB_URL = env.VITE_WEB_URL
 
 export const SYSTEM_CAN_UNDER_BLUR_WINDOW = globalThis?.window?.electron ?
-  window.api?.canWindowBlur :
+  globalThis?.window.api?.canWindowBlur :
   false
