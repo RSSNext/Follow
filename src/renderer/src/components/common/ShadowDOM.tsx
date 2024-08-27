2a9c67035576d6fab10fee9fb7a5cfd8b8d98e08
Innei <i@innei.in>
2024-08-27 14:50:25 +0800

    update
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/components/common/ShadowDOM.tsx
+++ src/renderer/src/components/common/ShadowDOM.tsx
@@ -1,6 +1,6 @@
 import { useIsDark } from "@renderer/hooks/common"
-import type { FC, PropsWithChildren } from "react"
-import { createContext, useContext, useState } from "react"
+import type { FC, PropsWithChildren, ReactNode } from "react"
+import { createContext, createElement, useContext, useState } from "react"
 import root from "react-shadow"
 
 const ShadowDOMContext = createContext(false)
@@ -9,23 +9,28 @@ export const ShadowDOM: FC<PropsWithChildren<React.HTMLProps<HTMLElement>>> & {
 } = (props) => {
   const { ...rest } = props
 
-  const [stylesElements] = useState<HTMLStyleElement[]>(() => [
-    ...document.head.querySelectorAll("style").values(),
-  ])
+  const [stylesElements] = useState<ReactNode[]>(() => {
+    const $styles = document.head.querySelectorAll("style").values()
+    const reactNodes = [] as ReactNode[]
+    let i = 0
+    for (const style of $styles) {
+      const key = `style-${i++}`
+      reactNodes.push(
+        createElement("style", {
+          key,
+          dangerouslySetInnerHTML: { __html: style.innerHTML },
+        }),
+      )
+    }
+    return reactNodes
+  })
 
   const dark = useIsDark()
   return (
     <root.div {...rest}>
       <ShadowDOMContext.Provider value={true}>
         <html data-theme={dark ? "dark" : "light"}>
-          <head>
-            {stylesElements.map((styleElement, index) => (
-              <style
-                key={index}
-                dangerouslySetInnerHTML={{ __html: styleElement.innerHTML }}
-              />
-            ))}
-          </head>
+          <head>{stylesElements}</head>
           {props.children}
         </html>
       </ShadowDOMContext.Provider>
