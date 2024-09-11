41caf4f744d2d39a6b98511171afb1c70dc7626d
Innei <i@innei.in>
2024-09-11 17:49:55 +0800

    chore: code style
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/providers/i18n-provider.tsx
+++ src/renderer/src/providers/i18n-provider.tsx
@@ -6,10 +6,12 @@ import { I18nextProvider } from "react-i18next"
 
 export const I18nProvider: FC<PropsWithChildren> = ({ children }) => {
   const [currentI18NInstance, update] = useState(i18next)
-  useEffect(() => {
-    return EventBus.subscribe("I18N_UPDATE", () => {
-      update(i18next.cloneInstance())
-    })
-  }, [])
+  useEffect(
+    () =>
+      EventBus.subscribe("I18N_UPDATE", () => {
+        update(i18next.cloneInstance())
+      }),
+    [],
+  )
   return <I18nextProvider i18n={currentI18NInstance}>{children}</I18nextProvider>
 }
