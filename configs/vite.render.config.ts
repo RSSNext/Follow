9024b6a497c7a65bf619e18fb2e4438741fea23f
Innei <i@innei.in>
2024-09-12 12:10:11 +0800

    chore: extract i18n hmr plugin to base
    Signed-off-by: Innei <i@innei.in>

--- configs/vite.render.config.ts
+++ configs/vite.render.config.ts
@@ -52,6 +52,25 @@ export const viteRenderBaseConfig = {
     }),
 
     viteTwToRawString(),
+
+    {
+      name: "custom-i18n-hmr",
+      handleHotUpdate({ file, server }) {
+        if (file.endsWith(".json") && file.includes("locales")) {
+          server.ws.send({
+            type: "custom",
+            event: "i18n-update",
+            data: {
+              file,
+              content: readFileSync(file, "utf-8"),
+            },
+          })
+
+          // return empty array to prevent the default HMR
+          return []
+        }
+      },
+    },
   ],
   define: {
     APP_VERSION: JSON.stringify(pkg.version),
