55266ce2cceea4e348c33989398f8f9bb92644a7
Innei <i@innei.in>
2024-09-12 21:53:45 +0800

    fix: tooltip in dark mode shadow
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/components/ui/tooltip/styles.ts
+++ src/renderer/src/components/ui/tooltip/styles.ts
@@ -5,5 +5,6 @@ export const tooltipStyle = {
     "rounded-lg text-sm",
     "max-w-[75ch] select-text",
     "drop-shadow data-[side=top]:shadow-tooltip-bottom data-[side=bottom]:shadow-tooltip-top",
+    tw`dark:drop-shadow-[0_0_1px_theme(colors.white/0.5)]`,
   ],
 }
