aeda63c29210df78aa00c789fa3ffaaa8a189878
upupnoah <upupqi.cs@gmail.com>
2024-09-14 17:42:55 +0700

    feat(i18n): add Japanese language support
    Added Japanese translations to the application, including localization for app content, common phrases, external resources, language settings, and shortcuts.

--- src/renderer/src/@types/constants.ts
+++ src/renderer/src/@types/constants.ts
@@ -1,11 +1,9 @@
 const langs = ["en", "ja", "zh-CN"]
 export const currentSupportedLanguages = langs.sort()
-
 export const dayjsLocaleImportMap = {
   en: ["en", () => import("dayjs/locale/en")],
   ["zh-CN"]: ["zh-cn", () => import("dayjs/locale/zh-cn")],
   ["ja"]: ["ja", () => import("dayjs/locale/ja")],
 }
-
 export const ns = ["app", "common", "lang", "settings", "shortcuts"] as const
 export const defaultNS = "app" as const
