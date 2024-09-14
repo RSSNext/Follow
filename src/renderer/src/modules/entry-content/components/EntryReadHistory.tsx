7d06fd7bdc8397f7d99ec1641fec2e1a8c95e3f0
Innei <i@innei.in>
2024-09-14 12:13:30 +0800

    feat: re-design style
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/modules/entry-content/components/EntryReadHistory.tsx
+++ src/renderer/src/modules/entry-content/components/EntryReadHistory.tsx
@@ -1,11 +1,11 @@
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardPortal,
+  HoverCardTrigger,
+} from "@radix-ui/react-hover-card"
 import { useWhoami } from "@renderer/atoms/user"
 import { Avatar, AvatarFallback, AvatarImage } from "@renderer/components/ui/avatar"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@renderer/components/ui/dropdown-menu/dropdown-menu"
 import {
   Tooltip,
   TooltipContent,
@@ -16,6 +16,7 @@ import { useAuthQuery } from "@renderer/hooks/common"
 import { Queries } from "@renderer/queries"
 import { useEntryReadHistory } from "@renderer/store/entry"
 import { useUserById } from "@renderer/store/user"
+import clsx from "clsx"
 import { LayoutGroup, m } from "framer-motion"
 import { memo, useEffect, useState } from "react"
 
@@ -60,39 +61,45 @@ export const EntryReadHistory: Component<{ entryId: string }> = ({ entryId }) =>
         entryHistory.readCount > 10 &&
         entryHistory.userIds &&
         entryHistory.userIds.length >= 10 && (
-          <DropdownMenu>
-            <Tooltip>
-              <TooltipTrigger className="no-drag-region relative cursor-pointer" asChild>
-                <DropdownMenuTrigger asChild>
-                  <button
-                    type="button"
-                    style={{
-                      right: "80px",
-                      zIndex: 11,
-                    }}
-                    className="relative flex size-7 items-center justify-center rounded-full border border-border bg-muted ring ring-background"
-                  >
-                    <span className="text-[10px] font-medium text-muted-foreground">
-                      +{Math.min(entryHistory.readCount - 10, 99)}
-                    </span>
-                  </button>
-                </DropdownMenuTrigger>
-              </TooltipTrigger>
-              <TooltipPortal>
-                <TooltipContent side="top">More</TooltipContent>
-              </TooltipPortal>
-            </Tooltip>
-            <DropdownMenuContent>
-              {entryHistory.userIds
-                .filter((id) => id !== me?.id)
-                .slice(10)
-                .map((userId) => (
-                  <DropdownMenuItem key={userId}>
-                    <EntryRow userId={userId} />
-                  </DropdownMenuItem>
-                ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <HoverCard>
+            <HoverCardTrigger asChild>
+              <button
+                type="button"
+                style={{
+                  right: "80px",
+                  zIndex: 11,
+                }}
+                className="relative flex size-7 items-center justify-center rounded-full border border-border bg-muted ring ring-background"
+              >
+                <span className="text-[10px] font-medium text-muted-foreground">
+                  +{Math.min(entryHistory.readCount - 10, 99)}
+                </span>
+              </button>
+            </HoverCardTrigger>
+
+            <HoverCardPortal>
+              <HoverCardContent
+                sideOffset={12}
+                align="start"
+                side="right"
+                asChild
+                className={clsx(
+                  "flex max-h-[300px] flex-col overflow-y-auto rounded-md border bg-background drop-shadow",
+                  // Animation, fade up
+                  "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=open]:slide-in-from-bottom-3",
+                )}
+              >
+                <ul>
+                  {entryHistory.userIds
+                    .filter((id) => id !== me?.id)
+                    .slice(10)
+                    .map((userId) => (
+                      <EntryRow userId={userId} key={userId} />
+                    ))}
+                </ul>
+              </HoverCardContent>
+            </HoverCardPortal>
+          </HoverCard>
         )}
     </div>
   )
@@ -100,24 +107,26 @@ export const EntryReadHistory: Component<{ entryId: string }> = ({ entryId }) =>
 
 const EntryRow: Component<{ userId: string }> = memo(({ userId }) => {
   const user = useUserById(userId)
-  const presentUserProfile = usePresentUserProfileModal("dialog")
+  const presentUserProfile = usePresentUserProfileModal("drawer")
   if (!user) return null
 
   return (
-    <button
-      type="button"
-      className="flex items-center gap-4"
-      onClick={() => {
-        presentUserProfile(userId)
-      }}
-    >
-      <Avatar className="aspect-square size-10 overflow-hidden rounded-full border border-border">
-        <AvatarImage src={user?.image || undefined} />
-        <AvatarFallback>{user.name?.slice(0, 2)}</AvatarFallback>
-      </Avatar>
+    <li className="rounded-md p-1 pr-4 hover:bg-muted">
+      <button
+        type="button"
+        className="flex items-center gap-2"
+        onClick={() => {
+          presentUserProfile(userId)
+        }}
+      >
+        <Avatar className="aspect-square size-7 overflow-hidden rounded-full border border-border ring-1 ring-background">
+          <AvatarImage src={user?.image || undefined} />
+          <AvatarFallback>{user.name?.slice(0, 2)}</AvatarFallback>
+        </Avatar>
 
-      {user.name && <p className="text-sm">{user.name}</p>}
-    </button>
+        {user.name && <p className="text-xs font-medium">{user.name}</p>}
+      </button>
+    </li>
   )
 })
 
