d624c43d1b9efabd927db4ff9af79afe009cf9a0
Noah <130611693+upupnoah@users.noreply.github.com>
2024-09-14 18:32:07 +0700

    feat(i18n): add Portuguese language support (#406)
    Added Portuguese translations to the application, including localization for app content, common phrases, external resources, language settings, and shortcuts.
    
    Co-authored-by: Innei <i@innei.in>

--- src/renderer/src/@types/constants.ts
+++ src/renderer/src/@types/constants.ts
@@ -1,9 +1,10 @@
-const langs = ["en", "ja", "zh-CN"]
+const langs = ["en", "ja", "zh-CN", "pt"]
 export const currentSupportedLanguages = langs.sort()
 export const dayjsLocaleImportMap = {
   en: ["en", () => import("dayjs/locale/en")],
   ["zh-CN"]: ["zh-cn", () => import("dayjs/locale/zh-cn")],
   ["ja"]: ["ja", () => import("dayjs/locale/ja")],
+  ["pt"]: ["pt", () => import("dayjs/locale/pt")],
 }
 export const ns = ["app", "common", "lang", "settings", "shortcuts"] as const
 export const defaultNS = "app" as const
