ba551ae56bd1ca5e5d4448e1e47d9ee3ed53c2e9
upupnoah <upupqi.cs@gmail.com>
2024-09-14 17:23:33 +0700

    feat(i18n): add Russian language support
    Added Russian translations to the application, including localization for app content, common phrases, external resources, language settings, and shortcuts.

--- src/renderer/src/@types/constants.ts
+++ src/renderer/src/@types/constants.ts
@@ -1,11 +1,10 @@
-const langs = ["en", "ja", "zh-CN"]
+const langs = ["en", "ja", "zh-CN", "ru"]
 export const currentSupportedLanguages = langs.sort()
-
 export const dayjsLocaleImportMap = {
   en: ["en", () => import("dayjs/locale/en")],
   ["zh-CN"]: ["zh-cn", () => import("dayjs/locale/zh-cn")],
   ["ja"]: ["ja", () => import("dayjs/locale/ja")],
+  ["ru"]: ["ru", () => import("dayjs/locale/ru")],
 }
-
 export const ns = ["app", "common", "lang", "settings", "shortcuts"] as const
 export const defaultNS = "app" as const
--- src/renderer/src/@types/default-resource.ts
+++ src/renderer/src/@types/default-resource.ts
@@ -1,14 +1,15 @@
 import en from "../../../../locales/app/en.json"
 import common_en from "../../../../locales/common/en.json"
 import common_ja from "../../../../locales/common/ja.json"
+import common_ru from "../../../../locales/common/ru.json"
 import common_zhCN from "../../../../locales/common/zh-CN.json"
 import external_en from "../../../../locales/external/en.json"
 import lang_en from "../../../../locales/lang/en.json"
 import lang_ja from "../../../../locales/lang/ja.json"
+import lang_ru from "../../../../locales/lang/ru.json"
 import lang_zhCN from "../../../../locales/lang/zh-CN.json"
 import settings_en from "../../../../locales/settings/en.json"
 import shortcuts_en from "../../../../locales/shortcuts/en.json"
-
 /**
  * This file is the language resource that is loaded in full when the app is initialized.
  * When switching languages, the app will automatically download the required language resources,
@@ -16,7 +17,6 @@ import shortcuts_en from "../../../../locales/shortcuts/en.json"
  * Generally, we only load english resources synchronously by default.
  * In addition, we attach common resources for other languages, and the size of the common resources must be controlled.
  */
-
 export const defaultResources = {
   en: {
     app: en,
@@ -34,4 +34,5 @@ export const defaultResources = {
     lang: lang_ja,
     common: common_ja,
   },
+  ru: { lang: lang_ru, common: common_ru },
 }
