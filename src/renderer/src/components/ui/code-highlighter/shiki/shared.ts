590d9cbb94a63f843d00bf54848bba40157d2452
Innei <i@innei.in>
2024-09-15 19:17:31 +0800

    feat: improve shiki code block renderer and  show language
    Signed-off-by: Innei <i@innei.in>

--- src/renderer/src/components/ui/code-highlighter/shiki/shared.ts
+++ src/renderer/src/components/ui/code-highlighter/shiki/shared.ts
@@ -4,9 +4,17 @@ import {
   transformerNotationHighlight,
 } from "@shikijs/transformers"
 import type { ShikiTransformer } from "shiki"
+import { createHighlighterCoreSync, createJavaScriptRegexEngine } from "shiki"
 
 export const shikiTransformers: ShikiTransformer[] = [
   transformerMetaHighlight(),
   transformerNotationDiff(),
   transformerNotationHighlight(),
 ]
+
+const js = createJavaScriptRegexEngine()
+export const shiki = createHighlighterCoreSync({
+  themes: [],
+  langs: [],
+  engine: js,
+})
