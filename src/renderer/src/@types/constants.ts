0aa8e6f37d8952ff59c077a43bca99e00e5ec826
Red Huang <redhuang@synology.com>
2024-09-15 13:22:48 +0800

    Translation: Traditional Chinese (#398)

--- src/renderer/src/@types/constants.ts
+++ src/renderer/src/@types/constants.ts
@@ -1,10 +1,11 @@
-const langs = ["en", "ja", "zh-CN", "pt"]
+const langs = ["en", "ja", "zh-CN", "zh-TW", "pt"]
 export const currentSupportedLanguages = langs.sort()
 export const dayjsLocaleImportMap = {
   en: ["en", () => import("dayjs/locale/en")],
   ["zh-CN"]: ["zh-cn", () => import("dayjs/locale/zh-cn")],
   ["ja"]: ["ja", () => import("dayjs/locale/ja")],
   ["pt"]: ["pt", () => import("dayjs/locale/pt")],
+  ["zh-TW"]: ["zh-tw", () => import("dayjs/locale/zh-tw")],
 }
 export const ns = ["app", "common", "lang", "settings", "shortcuts"] as const
 export const defaultNS = "app" as const
