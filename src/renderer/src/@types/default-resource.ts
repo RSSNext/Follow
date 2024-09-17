5bc360f2dc16b5f6c8fd2f10c0c588be0a6112cc
upupnoah <upupqi.cs@gmail.com>
2024-09-17 23:19:30 +0700

    feat(i18n): add ar-kw language support
    Added ar-kw translations to the application, including localization for app content, common phrases, external resources, language settings, and shortcuts.

--- src/renderer/src/@types/default-resource.ts
+++ src/renderer/src/@types/default-resource.ts
@@ -1,6 +1,7 @@
 import en from "../../../../locales/app/en.json"
 import common_ardz from "../../../../locales/common/ar-dz.json"
 import common_ariq from "../../../../locales/common/ar-iq.json"
+import common_arkw from "../../../../locales/common/ar-kw.json"
 import common_arma from "../../../../locales/common/ar-ma.json"
 import common_arsa from "../../../../locales/common/ar-sa.json"
 import common_en from "../../../../locales/common/en.json"
@@ -13,6 +14,7 @@ import common_zhTW from "../../../../locales/common/zh-TW.json"
 import external_en from "../../../../locales/external/en.json"
 import lang_ardz from "../../../../locales/lang/ar-dz.json"
 import lang_ariq from "../../../../locales/lang/ar-iq.json"
+import lang_arkw from "../../../../locales/lang/ar-kw.json"
 import lang_arma from "../../../../locales/lang/ar-ma.json"
 import lang_arsa from "../../../../locales/lang/ar-sa.json"
 import lang_en from "../../../../locales/lang/en.json"
@@ -56,4 +58,5 @@ export const defaultResources = {
   fr: { lang: lang_fr, common: common_fr },
   pt: { lang: lang_pt, common: common_pt },
   "ar-iq": { lang: lang_ariq, common: common_ariq },
+  "ar-kw": { lang: lang_arkw, common: common_arkw },
 }
