235b1d6b236bc5b6094671429b898143bdee4275
cloudflypeng <cloudflypeng@gmail.com>
2024-09-19 23:34:46 +0800

    feat: change corner-player animation

--- apps/renderer/src/modules/feed-column/corner-player.tsx
+++ apps/renderer/src/modules/feed-column/corner-player.tsx
@@ -2,7 +2,7 @@ import * as Slider from "@radix-ui/react-slider"
 import type { TooltipContentProps } from "@radix-ui/react-tooltip"
 import dayjs from "dayjs"
 import { AnimatePresence, m } from "framer-motion"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import Marquee from "react-fast-marquee"
 import { useHotkeys } from "react-hotkeys-hook"
 import { useTranslation } from "react-i18next"
@@ -144,7 +144,7 @@ const CornerPlayerImpl = () => {
           >
             {entry.entries.title}
           </Marquee>
-          <div className="mt-0.5 overflow-hidden truncate text-xs text-muted-foreground group-hover:mx-8">
+          <div className="mt-0.5 overflow-hidden truncate text-xs text-muted-foreground group-hover:opacity-0">
             {feed.title}
           </div>
 
@@ -154,7 +154,11 @@ const CornerPlayerImpl = () => {
       </div>
 
       {/* advanced controls */}
-      <div className="absolute inset-x-0 top-0 z-[-1] flex justify-between border-t bg-theme-modal-background-opaque p-1 opacity-0 transition-all duration-200 ease-in-out group-hover:-translate-y-full group-hover:opacity-100">
+      <div
+        className="absolute inset-x-0 top-0 z-[-1] flex justify-between border-t bg-theme-modal-background-opaque
+       p-1 opacity-100 transition-all duration-200 ease-in-out
+       group-hover:-translate-y-full group-hover:opacity-100"
+      >
         <div className="flex items-center">
           <ActionIcon
             className="i-mgc-close-cute-re"
@@ -172,8 +176,6 @@ const CornerPlayerImpl = () => {
             }
             label={t("player.open_entry")}
           />
-        </div>
-        <div className="flex items-center">
           <ActionIcon
             label={t("player.download")}
             onClick={() => {
@@ -182,6 +184,9 @@ const CornerPlayerImpl = () => {
           >
             <i className="i-mgc-download-2-cute-re" />
           </ActionIcon>
+        </div>
+        <div className="flex items-center">
+          {/* 音频相关 */}
           <ActionIcon label={<PlaybackRateSelector />} labelDelayDuration={0}>
             <PlaybackRateButton />
           </ActionIcon>
@@ -222,20 +227,21 @@ const PlayerProgress = () => {
     setControlledCurrentTime(currentTime)
   }, [currentTime, isDraggingProgress])
 
-  const currentTimeIndicator = dayjs()
-    .startOf("y")
-    .second(controlledCurrentTime)
-    .format(controlledCurrentTime > ONE_HOUR_IN_SECONDS ? "H:mm:ss" : "m:ss")
+  const getTimeIndicator = useCallback((time: number) => {
+    return dayjs()
+      .startOf("y")
+      .second(time)
+      .format(time > ONE_HOUR_IN_SECONDS ? "H:mm:ss" : "m:ss")
+  }, [])
+
+  const currentTimeIndicator = getTimeIndicator(controlledCurrentTime)
   const remainingTimeIndicator = duration
-    ? dayjs()
-        .startOf("y")
-        .second(duration - controlledCurrentTime)
-        .format(duration - controlledCurrentTime > ONE_HOUR_IN_SECONDS ? "H:mm:ss" : "m:ss")
+    ? getTimeIndicator(duration - controlledCurrentTime)
     : null
 
   return (
     <div className="relative mt-2">
-      <div className="absolute bottom-1 flex w-full items-center justify-between text-theme-disabled opacity-0 duration-200 ease-in-out group-hover:opacity-100">
+      <div className="absolute bottom-2 flex w-full items-center justify-between text-theme-disabled opacity-0 duration-150 ease-in-out group-hover:opacity-100">
         <div className="text-xs">{currentTimeIndicator}</div>
         {!!remainingTimeIndicator && <div className="text-xs">-{remainingTimeIndicator}</div>}
       </div>
@@ -259,7 +265,7 @@ const PlayerProgress = () => {
 
           {/* indicator */}
           <Slider.Thumb
-            className="block h-2 w-[3px] rounded-[1px] bg-accent"
+            className="block h-2 w-[3px] cursor-pointer rounded-[1px] bg-accent duration-200 ease-in-out group-hover:size-2"
             aria-label="Progress"
           />
         </Slider.Root>
