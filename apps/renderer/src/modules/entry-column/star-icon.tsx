b993ae050e282c22e40f649141677f7905ede601
legao <837937787@qq.com>
2024-09-26 06:53:52 +0000

    fix: cls when star in gird item (#636)
    * fix: grid item UI
    
    The star icon causes layout shift, and the original line height settings may lead to the title being truncated
    
    * fix: cls
    
    Signed-off-by: Innei <i@innei.in>
    
    * fix: hono
    
    Signed-off-by: Innei <i@innei.in>
    
    ---------
    
    Signed-off-by: Innei <i@innei.in>
    Co-authored-by: Innei <i@innei.in>

--- apps/renderer/src/modules/entry-column/star-icon.tsx
+++ apps/renderer/src/modules/entry-column/star-icon.tsx
@@ -1,7 +1,5 @@
 import { cn } from "~/lib/utils"
 
 export const StarIcon: Component = ({ className }) => (
-  <i
-    className={cn("i-mgc-star-cute-fi absolute right-0 top-0 text-base text-orange-400", className)}
-  />
+  <i className={cn("i-mgc-star-cute-fi text-base text-orange-400", className)} />
 )
