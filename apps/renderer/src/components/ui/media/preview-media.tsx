9239483081e6e4adfda4934695c03df394e873fa
Innei <i@innei.in>
2024-09-27 19:21:29 +0800

    fix: image blurhash `aspectRatio`
    Signed-off-by: Innei <i@innei.in>

--- apps/renderer/src/components/ui/media/preview-media.tsx
+++ apps/renderer/src/components/ui/media/preview-media.tsx
@@ -287,13 +287,9 @@ const FallbackableImage: FC<
       {isLoading && !isAllError && (
         <div className="center absolute inset-0 size-full">
           {blurhash ? (
-            <Blurhash
-              hash={blurhash}
-              resolutionX={32}
-              resolutionY={32}
-              className="!size-full"
-              style={{ aspectRatio: `${props.width} / ${props.height}` }}
-            />
+            <div style={{ aspectRatio: `${props.width} / ${props.height}` }} className="w-full">
+              <Blurhash hash={blurhash} resolutionX={32} resolutionY={32} className="!size-full" />
+            </div>
           ) : (
             <i className="i-mgc-loading-3-cute-re size-8 animate-spin text-white/80" />
           )}
