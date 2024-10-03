ebceaf6be4f702bab22749a4305bf52174200dfc
Wayne Zhang <waynzh19@gmail.com>
2024-10-03 11:48:42 +0800

    fix(styles): align the styles of Skeleton and Item (#749)

--- apps/renderer/src/modules/entry-column/Items/social-media-item.tsx
+++ apps/renderer/src/modules/entry-column/Items/social-media-item.tsx
@@ -206,9 +206,9 @@ const ActionBar = ({ entryId }: { entryId: string }) => {
 }
 
 export const SocialMediaItemSkeleton = (
-  <div className="relative m-auto w-[75ch] rounded-md bg-theme-background text-zinc-700 transition-colors dark:text-neutral-400">
+  <div className="relative m-auto w-[645px] rounded-md bg-theme-background text-zinc-700 transition-colors dark:text-neutral-400">
     <div className="relative">
-      <div className="group relative flex py-4 pl-3 pr-2">
+      <div className="group relative flex px-8 py-6">
         <Skeleton className="mr-2 size-9" />
         <div className="ml-2 min-w-0 flex-1">
           <div className="-mt-0.5 line-clamp-5 flex-1 text-sm">
