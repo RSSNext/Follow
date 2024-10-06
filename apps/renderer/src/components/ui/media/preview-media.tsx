df41edc054cd7ae8b78779d343bc9bdc52725c5a
cesaryuan <cesaryuan@qq.com>
2024-10-06 10:03:39 +0800

    fix: add props.style

--- apps/renderer/src/components/ui/media/preview-media.tsx
+++ apps/renderer/src/components/ui/media/preview-media.tsx
@@ -321,7 +321,9 @@ const FallbackableImage: FC<
           )}
           style={
             Number.isNaN(height) || Number.isNaN(width) || height === 0 || width === 0
-              ? {}
+              ? {
+                  ...props.style,
+                }
               : {
                   maxHeight: `min(100%, ${height}px)`,
                   maxWidth: `min(100%, ${width}px)`,
