2606b43ca8cf65da5d6fda59854d4dc9f2ce2e38
Innei <Innei@users.noreply.github.com>
2024-10-09 05:05:26 +0000

    chore: auto-fix linting and formatting issues

--- apps/renderer/src/components/icons/crown.tsx
+++ apps/renderer/src/components/icons/crown.tsx
@@ -11,7 +11,7 @@ export function IconoirBrightCrown(props: SVGProps<SVGSVGElement>) {
         strokeLinejoin="round"
         strokeWidth="1.5"
         d="M22 12h1M12 2V1m0 22v-1m8-2l-1-1m1-15l-1 1M4 20l1-1M4 4l1 1m-4 7h1m14.8 3.5l1.2-7l-4.2 2.1L12 8.5l-1.8 2.1L6 8.5l1.2 7z"
-       />
+      />
     </svg>
   )
 }
