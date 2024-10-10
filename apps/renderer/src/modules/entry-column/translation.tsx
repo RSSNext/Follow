71abbc9b8e0954b0ef6b4d65709b40222b8acc81
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-10-10 22:29:42 +0800

    fix: translation tooltip z index

--- apps/renderer/src/modules/entry-column/translation.tsx
+++ apps/renderer/src/modules/entry-column/translation.tsx
@@ -50,7 +50,7 @@ export const EntryTranslation: Component<{
       <HoverCard.Portal>
         <HoverCard.Content
           className={cn(
-            "group relative z-[1] text-sm",
+            "group relative z-[11] text-sm",
             "animate-in fade-in-0 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
           )}
           style={useOverlay ? { width: bounds.width } : undefined}
