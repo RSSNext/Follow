134e2ba53ace354332ca5d22af09c4d47e901190
Innei <tukon479@gmail.com>
2024-10-16 20:50:31 +0800

    update
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/server/api/index.ts
+++ apps/server/api/index.ts
@@ -1,7 +1,6 @@
-// import { createApp } from "../dist/server/index.js"
 const { createApp } = require("../dist/server/index.js")
 
-export default async function handler(req: any, res: any) {
+module.exports = async function handler(req: any, res: any) {
   const app = await createApp()
   await app.ready()
   app.server.emit("request", req, res)
