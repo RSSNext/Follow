70014559200601bdecacc1cacf8e22dd609feeb3
lawvs <18554747+lawvs@users.noreply.github.com>
2024-10-23 13:45:10 +0800

    feat: add boost certification to feed list

--- apps/renderer/src/modules/boost/hooks.tsx
+++ apps/renderer/src/modules/boost/hooks.tsx
@@ -1,7 +1,9 @@
+import { atom } from "jotai"
 import { useCallback } from "react"
 
 import { useAsyncModal } from "~/components/ui/modal/helper/use-async-modal"
 import { useAuthQuery, useI18n } from "~/hooks/common"
+import { createAtomHooks } from "~/lib/jotai"
 import { boosts } from "~/queries/boosts"
 
 import { BoostModalContent } from "./modal"
@@ -26,4 +28,18 @@ export const useBoostModal = () => {
     },
     [present, t],
   )
+}
+
+const [, , useFeedBoostMap, , getFeedBoostMap, setFeedBoostMap] = createAtomHooks(
+  atom<Record<string, boolean>>({}),
+)
+
+export const updateFeedBoostStatus = (feedId: string, isBoosted: boolean) => {
+  const prev = getFeedBoostMap()
+  setFeedBoostMap({ ...prev, [feedId]: isBoosted })
+}
+
+export const useIsFeedBoosted = (feedId: string) => {
+  const map = useFeedBoostMap()
+  return map[feedId] ?? false
 }
