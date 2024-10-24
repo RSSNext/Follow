d06da266788eda3f78fafd1369cdc5f143466cc1
lawvs <18554747+lawvs@users.noreply.github.com>
2024-10-24 19:06:35 +0800

    chore: limit booster list

--- apps/renderer/src/modules/boost/boosting-contributors.tsx
+++ apps/renderer/src/modules/boost/boosting-contributors.tsx
@@ -23,23 +23,26 @@ export const BoostingContributors = ({ feedId }: { feedId: string }) => {
     >
       <h2 className="mb-4 text-2xl font-bold">Boosting Contributors</h2>
       <ul className="space-y-4">
-        {boosters.map((user) => (
-          <li
-            key={user.id}
-            role="button"
-            className="flex items-center space-x-4"
-            onClick={() => presentUserProfile(user.id)}
-          >
-            <Avatar className="block aspect-square size-10 overflow-hidden rounded-full border border-border ring-1 ring-background">
-              <AvatarImage src={replaceImgUrlIfNeed(user?.image || undefined)} />
-              <AvatarFallback>{user.name?.slice(0, 2)}</AvatarFallback>
-            </Avatar>
-            <div>
-              <div className="font-semibold">{user.name || user.handle}</div>
-              {user.handle && <div className="text-xs text-gray-500">@{user.handle}</div>}
-            </div>
-          </li>
-        ))}
+        {boosters
+          .map((user) => (
+            <li
+              key={user.id}
+              role="button"
+              className="flex items-center space-x-4"
+              onClick={() => presentUserProfile(user.id)}
+            >
+              <Avatar className="block aspect-square size-10 overflow-hidden rounded-full border border-border ring-1 ring-background">
+                <AvatarImage src={replaceImgUrlIfNeed(user?.image || undefined)} />
+                <AvatarFallback>{user.name?.slice(0, 2)}</AvatarFallback>
+              </Avatar>
+              <div>
+                <div className="font-semibold">{user.name || user.handle}</div>
+                {user.handle && <div className="text-xs text-gray-500">@{user.handle}</div>}
+              </div>
+            </li>
+          ))
+          // TODO use grid layout when there are many boosters
+          .slice(0, 10)}
       </ul>
     </m.div>
   )
