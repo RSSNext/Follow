86be1a2e46d3298157cece0731e687a462f833d5
Innei <tukon479@gmail.com>
2024-10-25 22:06:28 +0800

    feat(discover): improve trending style and discover no image placeholer
    Signed-off-by: Innei <tukon479@gmail.com>

--- packages/types/global.d.ts
+++ packages/types/global.d.ts
@@ -3,6 +3,11 @@ declare global {
   type IsLiteralString<T> = T extends string ? (string extends T ? never : T) : never
 
   type OmitStringType<T> = T extends any[] ? OmitStringType<T[number]> : IsLiteralString<T>
+  type NonUndefined<T> = T extends undefined
+    ? never
+    : T extends object
+      ? { [K in keyof T]: NonUndefined<T[K]> }
+      : T
 
   export const APP_NAME = "Follow"
 }
--- packages/utils/src/color.ts
+++ packages/utils/src/color.ts
@@ -1,3 +1,5 @@
+import uniqolor from "uniqolor"
+
 const getRandomColor = (lightness: [number, number], saturation: [number, number], hue: number) => {
   const satAccent = Math.floor(Math.random() * (saturation[1] - saturation[0] + 1) + saturation[0])
   const lightAccent = Math.floor(Math.random() * (lightness[1] - lightness[0] + 1) + lightness[0])
@@ -51,4 +53,61 @@ export const getColorScheme = (hue?: number, memo?: boolean) => {
     },
   }
   return result
+}
+
+const lerp = (a: number, b: number, t: number) => a + (b - a) * t
+
+export const hexToRgb = (hex: string) => {
+  const bigint = Number.parseInt(hex.slice(1), 16)
+  return [(bigint >> 16) & 255, (bigint >> 8) & 255, bigint & 255]
+}
+
+export const rgbToHex = (r: number, g: number, b: number) => {
+  return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`
+}
+
+const adjustColorTowardsTarget = (color: string, targetColor: string, factor: number) => {
+  const [r1, g1, b1] = hexToRgb(color)
+  const [r2, g2, b2] = hexToRgb(targetColor)
+
+  const r = Math.round(lerp(r1, r2, factor))
+  const g = Math.round(lerp(g1, g2, factor))
+  const b = Math.round(lerp(b1, b2, factor))
+
+  return rgbToHex(r, g, b)
+}
+
+export const getBackgroundGradient = (seed?: Nullable<string>) => {
+  const nextSeed = seed ?? Math.random().toString(36).slice(7)
+
+  const bgAccent = uniqolor(nextSeed, {
+    saturation: [30, 35],
+    lightness: [60, 70],
+  }).color
+
+  const bgAccentLight = uniqolor(nextSeed, {
+    saturation: [30, 35],
+    lightness: [80, 90],
+  }).color
+
+  const bgAccentUltraLight = uniqolor(nextSeed, {
+    saturation: [30, 35],
+    lightness: [95, 96],
+  }).color
+
+  const targetColor = "#FF5C02"
+  const factor = 0.3 // Adjust this value to control how close the color gets to the target color
+
+  const adjustedAccent = adjustColorTowardsTarget(bgAccent, targetColor, factor)
+  const adjustedAccentLight = adjustColorTowardsTarget(bgAccentLight, targetColor, factor)
+  const adjustedAccentUltraLight = adjustColorTowardsTarget(bgAccentUltraLight, targetColor, factor)
+
+  return [
+    adjustedAccent,
+    adjustedAccentLight,
+    adjustedAccentUltraLight,
+    bgAccent,
+    bgAccentLight,
+    bgAccentUltraLight,
+  ]
 }
