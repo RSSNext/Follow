e3d109308a83c58fbbf91449039c3c75a18b2140
lawvs <18554747+lawvs@users.noreply.github.com>
2024-10-27 03:36:34 +0800

    feat: integrate UserGallery for displaying boosting contributors

--- apps/renderer/src/modules/boost/boosting-contributors.tsx
+++ apps/renderer/src/modules/boost/boosting-contributors.tsx
@@ -5,6 +5,8 @@ import { replaceImgUrlIfNeed } from "~/lib/img-proxy"
 import { useFeedBoostersQuery } from "~/modules/boost/query"
 
 import { usePresentUserProfileModal } from "../profile/hooks"
+import { UserGallery } from "../user/UserGallery"
+import type { User } from "../user/utils"
 
 export const BoostingContributors = ({ feedId }: { feedId: string }) => {
   const { data: boosters, isLoading } = useFeedBoostersQuery(feedId)
@@ -21,27 +23,34 @@ export const BoostingContributors = ({ feedId }: { feedId: string }) => {
     >
       <h2 className="mb-4 text-2xl font-bold">Boosting Contributors</h2>
       <ul className="space-y-4">
-        {boosters
-          .map((user) => (
-            <li
-              key={user.id}
-              role="button"
-              className="flex items-center space-x-4"
-              onClick={() => presentUserProfile(user.id)}
-            >
-              <Avatar className="block aspect-square size-10 overflow-hidden rounded-full border border-border ring-1 ring-background">
-                <AvatarImage src={replaceImgUrlIfNeed(user?.image || undefined)} />
-                <AvatarFallback>{user.name?.slice(0, 2)}</AvatarFallback>
-              </Avatar>
-              <div>
-                <div className="font-semibold">{user.name || user.handle}</div>
-                {user.handle && <div className="text-xs text-gray-500">@{user.handle}</div>}
-              </div>
-            </li>
+        {boosters.length < 8 ? (
+          boosters.map((user) => (
+            <UserListItem key={user.id} user={user} onClick={presentUserProfile} />
           ))
-          // TODO use grid layout when there are many boosters
-          .slice(0, 10)}
+        ) : (
+          <UserGallery users={boosters} />
+        )}
       </ul>
     </m.div>
+  )
+}
+
+const UserListItem = ({ user, onClick }: { user: User; onClick: (userId: string) => void }) => {
+  return (
+    <li
+      key={user.id}
+      role="button"
+      className="flex items-center space-x-4"
+      onClick={() => onClick(user.id)}
+    >
+      <Avatar className="block aspect-square size-10 overflow-hidden rounded-full border border-border ring-1 ring-background">
+        <AvatarImage src={replaceImgUrlIfNeed(user?.image || undefined)} />
+        <AvatarFallback>{user.name?.slice(0, 2)}</AvatarFallback>
+      </Avatar>
+      <div>
+        <div className="font-semibold">{user.name || user.handle}</div>
+        {user.handle && <div className="text-xs text-gray-500">@{user.handle}</div>}
+      </div>
+    </li>
   )
 }
