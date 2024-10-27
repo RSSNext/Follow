ef46c70a9849c315cc8d827f4ad8a3c004003bac
Innei <tukon479@gmail.com>
2024-10-27 23:12:40 +0800

    fix(external): set auth config first
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/server/client/initialize/index.ts
+++ apps/server/client/initialize/index.ts
@@ -1,9 +1,17 @@
 import { initI18n } from "@client/i18n"
+import { env } from "@follow/shared/env"
+import { authConfigManager } from "@hono/auth-js/react"
 
 import { initAnalytics } from "./analytics"
 import { initSentry } from "./sentry"
 
-export const initialize = async () => {
+export const initialize = () => {
+  authConfigManager.setConfig({
+    baseUrl: env.VITE_API_URL,
+    basePath: "/auth",
+    credentials: "include",
+  })
+
   initI18n()
   initAnalytics()
   initSentry()
