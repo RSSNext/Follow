23e767dce40a01e099c24a0ce4ec800a141d2ec5
lawvs <18554747+lawvs@users.noreply.github.com>
2024-11-03 03:11:51 +0800

    refactor: simplify native menu

--- apps/renderer/src/lib/native-menu.ts
+++ apps/renderer/src/lib/native-menu.ts
@@ -1,165 +1,54 @@
-import { IN_ELECTRON } from "@follow/shared/constants"
-import { getOS } from "@follow/utils/utils"
-import { get } from "lodash-es"
+import type { MenuItemConstructorOptions } from "electron"
 
 import { tipcClient } from "./client"
 
-type MenuItemWithHide<T> = T & {
-  hide?: boolean
-}
-
-type BaseMenuItemText = MenuItemWithHide<{
-  type: "text"
-  label: string
+export type ElectronMenuItem = Omit<MenuItemConstructorOptions, "click" | "submenu"> & {
   click?: () => void
-  /** only work in web app */
-  icon?: React.ReactNode
-  shortcut?: string
-  disabled?: boolean
-  checked?: boolean
-  supportMultipleSelection?: boolean
-}>
-
-type BaseMenuItemSeparator = MenuItemWithHide<{
-  type: "separator"
-  disabled?: boolean
-}>
-
-type BaseMenuItem = BaseMenuItemText | BaseMenuItemSeparator
-
-export type NativeMenuItem = BaseMenuItem & {
-  submenu?: NativeMenuItem[]
+  submenu?: ElectronMenuItem[]
 }
 
-export type NullableNativeMenuItem =
-  | (BaseMenuItemText & { submenu?: NullableNativeMenuItem[] })
-  | BaseMenuItemSeparator
-  | null
-  | undefined
-  | false
-  | ""
-
-function sortShortcutsString(shortcut: string) {
-  const order = ["Shift", "Ctrl", "Meta", "Alt"]
-  let nextShortcut = shortcut
-  if (getOS() === "Windows") {
-    nextShortcut = shortcut.replace("Meta", "Ctrl").replace("meta", "ctrl")
-  }
-  const arr = nextShortcut.split("+")
-
-  const sortedModifiers = arr
-    .filter((key) => order.includes(key))
-    .sort((a, b) => order.indexOf(a) - order.indexOf(b))
-
-  const otherKeys = arr.filter((key) => !order.includes(key))
-
-  return [...sortedModifiers, ...otherKeys].join("+")
+export const showElectronContextMenu = async (items: Array<ElectronMenuItem>) => {
+  if (!window.electron) throw new Error("electron is not available")
+  const dispose = window.electron.ipcRenderer.on(
+    "menu-click",
+    (_, { path }: { path: number[] }) => {
+      const targetMenu = getMenuItemByPath(items, path)
+      if (targetMenu && typeof targetMenu.click === "function") {
+        targetMenu.click()
+      } else {
+        console.warn(`Menu item not found or click handler missing for path: ${path}`)
+      }
+    },
+  )
+  const itemsWithoutClick = removeClick(items)
+  await tipcClient?.showContextMenu({ items: itemsWithoutClick })
+  dispose()
 }
 
-function processMenuItems(items: NullableNativeMenuItem[]): NativeMenuItem[] {
-  return (items.filter((item) => item && !item.hide) as NativeMenuItem[]).map((item) => {
-    if (item.type === "text") {
+const removeClick = (item: ElectronMenuItem[]): ElectronMenuItem[] =>
+  item.map(({ click, ...rest }) => {
+    if (rest.submenu)
       return {
-        ...item,
-        shortcut: item.shortcut ? sortShortcutsString(item.shortcut) : undefined,
-        submenu: item.submenu ? processMenuItems(item.submenu) : undefined,
+        ...rest,
+        submenu: removeClick(rest.submenu),
       }
-    }
-    return item
+    return rest
   })
+
+// Function to retrieve the menu item based on the provided path
+const getMenuItemByPath = (items: ElectronMenuItem[], path: number[]): ElectronMenuItem | null => {
+  let currentItems = items
+  let currentItem: ElectronMenuItem | null = null
+
+  for (const index of path) {
+    if (!currentItems || index >= currentItems.length) return null
+    currentItem = currentItems[index]
+
+    if (currentItem.submenu && Array.isArray(currentItem.submenu)) {
+      currentItems = currentItem.submenu
+    } else {
+      currentItems = []
+    }
+  }
+  return currentItem
 }
-export const showNativeMenu = async (
-  items: Array<NullableNativeMenuItem>,
-  e?: MouseEvent | React.MouseEvent,
-) => {
-  const nextItems = processMenuItems(items)
-
-  const el = e && e.currentTarget
-
-  if (el instanceof HTMLElement) {
-    el.dataset.contextMenuOpen = "true"
-  }
-
-  // only show native menu on macOS electron, because in other platform, the native ui is not good
-  if (!IN_ELECTRON || getOS() !== "macOS") {
-    document.dispatchEvent(
-      new CustomEvent(CONTEXT_MENU_SHOW_EVENT_KEY, {
-        detail: {
-          items: nextItems,
-          x: e?.clientX,
-          y: e?.clientY,
-        },
-      }),
-    )
-    return
-  } else {
-    if (import.meta.env.DEV && e) {
-      nextItems.push(
-        {
-          type: "separator" as const,
-        },
-        {
-          type: "text" as const,
-          label: "Inspect Element",
-          click: () => {
-            tipcClient?.inspectElement({
-              x: e.pageX,
-              y: e.pageY,
-            })
-          },
-        },
-      )
-    }
-  }
-
-  const dispose = window.electron?.ipcRenderer.on("menu-click", (_, combinedIndex: string) => {
-    const arr = combinedIndex.split("-")
-    const accessors = [] as string[]
-    for (let i = 0; i < arr.length; i++) {
-      accessors.push(arr[i])
-
-      if (i !== arr.length - 1) {
-        accessors.push("submenu")
-      }
-    }
-    const item = get(nextItems, accessors)
-
-    if (item && item.type === "text") {
-      item.click?.()
-    }
-  })
-
-  window.electron?.ipcRenderer.once("menu-closed", () => {
-    dispose?.()
-    if (el instanceof HTMLElement) {
-      delete el.dataset.contextMenuOpen
-    }
-
-    // dispatch mouse move event
-    // NOTE: in order to remove the highlight of the trigger item
-    // e.g. https://vscode.dev/github/RSSNext/follow/blob/dev/apps/renderer/src/modules/entry-column/layouts/EntryItemWrapper.tsx#L80
-    document.dispatchEvent(new PointerEvent("pointerdown", { bubbles: true }))
-  })
-
-  await tipcClient?.showContextMenu({
-    items: transformMenuItemsForNative(nextItems),
-  })
-
-  function transformMenuItemsForNative(nextItems: NativeMenuItem[]) {
-    return nextItems.map((item) => {
-      if (item.type === "text") {
-        return {
-          ...item,
-          icon: undefined,
-          enabled: item.click !== undefined || (!!item.submenu && item.submenu.length > 0),
-          click: undefined,
-          submenu: item.submenu ? transformMenuItemsForNative(item.submenu) : undefined,
-          shortcut: item.shortcut?.replace("Meta", "CmdOrCtrl"),
-        }
-      }
-      return item
-    })
-  }
-}
-
-export const CONTEXT_MENU_SHOW_EVENT_KEY = "contextmenu-show"
