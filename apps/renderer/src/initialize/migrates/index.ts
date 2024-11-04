dd187d536b22a4566c0ee9942a4505e9c8628a89
lawvs <18554747+lawvs@users.noreply.github.com>
2024-11-04 18:34:24 +0800

    fix: handle tipUsers migration for older feed versions

--- apps/renderer/src/initialize/migrates/index.ts
+++ apps/renderer/src/initialize/migrates/index.ts
@@ -1,6 +1,8 @@
 import { getStorageNS } from "@follow/utils/ns"
+import * as semver from "semver"
 
 import { appLog } from "~/lib/log"
+import { FeedService } from "~/services/feed"
 
 const appVersionKey = getStorageNS("app_version")
 
@@ -9,14 +11,32 @@ declare global {
     __app_is_upgraded__: boolean
   }
 }
-export const doMigration = () => {
+
+async function migrationTipUser() {
+  // Add migration logic here
+  const feeds = await FeedService.findAll()
+  const dirtyFeed = feeds.filter((feed) => !!feed.tipUsers && !Array.isArray(feed.tipUsers))
+  dirtyFeed.forEach((feed) => {
+    feed.tipUsers = []
+  })
+  await FeedService.upsertMany(dirtyFeed)
+}
+
+export const doMigration = async () => {
   const lastVersion = localStorage.getItem(appVersionKey)
 
   if (lastVersion && lastVersion !== APP_VERSION) {
     appLog(`Upgrade from ${lastVersion} to ${APP_VERSION}`)
     window.__app_is_upgraded__ = true
 
-    // NOTE: Add migration logic here
+    try {
+      if (semver.lte(lastVersion, "0.1.2-beta.0")) {
+        await migrationTipUser()
+      }
+      // NOTE: Add migration logic here
+    } catch (error) {
+      console.error("Migration failed", error)
+    }
   }
   localStorage.setItem(appVersionKey, APP_VERSION)
 }
