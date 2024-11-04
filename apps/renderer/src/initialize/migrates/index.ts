1354bb3298435d88ef70dbe00f31b7edc9cf842c
lawvs <18554747+lawvs@users.noreply.github.com>
2024-11-05 01:23:10 +0800

    refactor: remove legacy migration function and its tests

--- apps/renderer/src/initialize/migrates/index.ts
+++ apps/renderer/src/initialize/migrates/index.ts
@@ -1,8 +1,6 @@
 import { getStorageNS } from "@follow/utils/ns"
-import * as semver from "semver"
 
 import { appLog } from "~/lib/log"
-import { FeedService } from "~/services/feed"
 
 const appVersionKey = getStorageNS("app_version")
 
@@ -12,30 +10,14 @@ declare global {
   }
 }
 
-async function migrationTipUser() {
-  // Fix https://github.com/RSSNext/Follow/issues/1308
-  const feeds = await FeedService.findAll()
-  const dirtyFeed = feeds.filter((feed) => !!feed.tipUsers && !Array.isArray(feed.tipUsers))
-  const newFeed = dirtyFeed.map((feed) => ({ ...feed, tipUsers: [] }))
-  await FeedService.upsertMany(newFeed)
-}
-
 export const doMigration = async () => {
   const lastVersion = localStorage.getItem(appVersionKey)
   if (!lastVersion || lastVersion === APP_VERSION) {
     localStorage.setItem(appVersionKey, APP_VERSION)
     return
   }
+  // NOTE: Add migration logic here
 
   appLog(`Upgrade from ${lastVersion} to ${APP_VERSION}`)
   window.__app_is_upgraded__ = true
-
-  try {
-    if (semver.lte(lastVersion, "0.1.2-beta.0")) {
-      await migrationTipUser()
-    }
-    // NOTE: Add migration logic here
-  } catch (error) {
-    console.error("Migration failed", error)
-  }
 }
