82c7fa4f0ab9ed724b4d28d71530455f809c035b
lawvs <18554747+lawvs@users.noreply.github.com>
2024-11-04 18:36:55 +0800

    chore: clean code

--- apps/renderer/src/initialize/migrates/index.ts
+++ apps/renderer/src/initialize/migrates/index.ts
@@ -13,30 +13,28 @@ declare global {
 }
 
 async function migrationTipUser() {
-  // Add migration logic here
   const feeds = await FeedService.findAll()
   const dirtyFeed = feeds.filter((feed) => !!feed.tipUsers && !Array.isArray(feed.tipUsers))
-  dirtyFeed.forEach((feed) => {
-    feed.tipUsers = []
-  })
-  await FeedService.upsertMany(dirtyFeed)
+  const newFeed = dirtyFeed.map((feed) => ({ ...feed, tipUsers: [] }))
+  await FeedService.upsertMany(newFeed)
 }
 
 export const doMigration = async () => {
   const lastVersion = localStorage.getItem(appVersionKey)
-
-  if (lastVersion && lastVersion !== APP_VERSION) {
-    appLog(`Upgrade from ${lastVersion} to ${APP_VERSION}`)
-    window.__app_is_upgraded__ = true
-
-    try {
-      if (semver.lte(lastVersion, "0.1.2-beta.0")) {
-        await migrationTipUser()
-      }
-      // NOTE: Add migration logic here
-    } catch (error) {
-      console.error("Migration failed", error)
-    }
+  if (!lastVersion || lastVersion === APP_VERSION) {
+    localStorage.setItem(appVersionKey, APP_VERSION)
+    return
   }
-  localStorage.setItem(appVersionKey, APP_VERSION)
+
+  appLog(`Upgrade from ${lastVersion} to ${APP_VERSION}`)
+  window.__app_is_upgraded__ = true
+
+  try {
+    if (semver.lte(lastVersion, "0.1.2-beta.0")) {
+      await migrationTipUser()
+    }
+    // NOTE: Add migration logic here
+  } catch (error) {
+    console.error("Migration failed", error)
+  }
 }
