8f2b10f9d0e57130c4dffc7ab61a6c71fc1bc908
lawvs <18554747+lawvs@users.noreply.github.com>
2024-11-06 01:19:35 +0800

    feat: enhance app tray functionality

--- apps/main/src/lib/tray.ts
+++ apps/main/src/lib/tray.ts
@@ -1,22 +1,64 @@
-import { app, nativeImage, Tray } from "electron"
+import { name } from "@pkg"
+import { app, Menu, nativeImage, Tray } from "electron"
 
+import { isMacOS } from "~/env"
 import { getIconPath } from "~/helper"
 
 import { getMainWindow } from "../window"
+import { t } from "./i18n"
+import { store } from "./store"
+
+// https://www.electronjs.org/docs/latest/tutorial/tray
+
+let tray: Tray | null = null
 
 export const registerAppTray = () => {
+  if (!getTrayConfig()) return
+
   const icon = nativeImage.createFromPath(getIconPath())
   // See https://stackoverflow.com/questions/41664208/electron-tray-icon-change-depending-on-dark-theme/41998326#41998326
   const trayIcon = icon.resize({ width: 16 })
-  const tray = new Tray(trayIcon)
+  // trayIcon.setTemplateImage(true)
+  tray = new Tray(trayIcon)
 
-  tray.on("click", () => {
-    const mainWindow = getMainWindow()
-    if (mainWindow?.isMinimized()) {
-      mainWindow.restore()
-    } else {
-      mainWindow?.show()
-    }
-  })
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: t("menu.open", { name }),
+      click: showWindow,
+    },
+    { label: t("menu.quit", { name }), click: () => app.quit() },
+  ])
+  tray.setContextMenu(contextMenu)
   tray.setToolTip(app.getName())
+  tray.on("click", showWindow)
+}
+
+const showWindow = () => {
+  const mainWindow = getMainWindow()
+  if (!mainWindow) return
+  if (mainWindow.isMinimized()) {
+    mainWindow.restore()
+  } else {
+    mainWindow.show()
+  }
+}
+
+const destroyAppTray = () => {
+  if (tray) {
+    tray.destroy()
+    tray = null
+  }
+}
+
+const DEFAULT_MINIMIZE_TO_TRAY = isMacOS ? false : true
+
+export const getTrayConfig = (): boolean => store.get("minimizeToTray") ?? DEFAULT_MINIMIZE_TO_TRAY
+
+export const setTrayConfig = (input: boolean) => {
+  store.set("minimizeToTray", input)
+  if (input) {
+    registerAppTray()
+  } else {
+    destroyAppTray()
+  }
 }
