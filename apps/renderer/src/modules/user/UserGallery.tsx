fdcced11b41c0768416c8104ef0acff51b751176
Eric Zhu <eric@zhu.email>
2024-11-07 16:42:18 +0800

    fix(renderer): remove nnecessary requests in UserGallery component

--- apps/renderer/src/modules/user/UserGallery.tsx
+++ apps/renderer/src/modules/user/UserGallery.tsx
@@ -1,4 +1,8 @@
-import { UserAvatar } from "~/modules/user/UserAvatar"
+import { Avatar, AvatarFallback, AvatarImage } from "@follow/components/ui/avatar/index.jsx"
+import { getColorScheme, stringToHue } from "@follow/utils/color"
+
+import { replaceImgUrlIfNeed } from "~/lib/img-proxy"
+import { usePresentUserProfileModal } from "~/modules/profile/hooks"
 
 import type { User } from "./utils"
 
@@ -15,15 +19,36 @@ export const UserGallery = ({ users, limit = 18 }: UserGalleryProps) => {
     <div className="mx-auto flex w-fit max-w-80 flex-wrap gap-4">
       {displayedUsers.map((user) => (
         <div key={user.id} className="size-8">
-          <UserAvatar
-            className="h-auto p-0"
-            avatarClassName="size-8"
-            userId={user?.id}
-            enableModal={true}
-            hideName={true}
-          />
+          <UserAvatar user={user} />
         </div>
       ))}
     </div>
+  )
+}
+
+const UserAvatar: Component<{
+  user: User | null
+}> = ({ user }) => {
+  const presentUserProfile = usePresentUserProfileModal("drawer")
+  const randomColor = stringToHue(user?.name || "")
+  if (!user) return null
+  return (
+    <Avatar
+      onClick={() => {
+        presentUserProfile(user.id)
+      }}
+      className="aspect-square size-8 h-full w-auto overflow-hidden rounded-full border bg-stone-300"
+    >
+      <AvatarImage
+        src={replaceImgUrlIfNeed(user?.image || undefined)}
+        className="duration-200 animate-in fade-in-0"
+      />
+      <AvatarFallback
+        style={{ backgroundColor: getColorScheme(randomColor, true).light.accent }}
+        className="text-xs text-white"
+      >
+        {user?.name?.[0]}
+      </AvatarFallback>
+    </Avatar>
   )
 }
