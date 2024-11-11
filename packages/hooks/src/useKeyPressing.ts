6b8572c37aa287aa544e6ddb2847693ba597b0be
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-11-11 11:40:59 +0800

    fix: allow selecting when meta key is pressed

---
+++ packages/hooks/src/useKeyPressing.ts
@@ -0,0 +1,29 @@
+import { useEffect, useMemo, useState } from "react"
+
+export function useKeyPressing(key: string) {
+  const [keysPressed, setKeysPressed] = useState(() => new Set())
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      setKeysPressed((prev) => new Set(prev).add(event.key))
+    }
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      setKeysPressed((prev) => {
+        const newSet = new Set(prev)
+        newSet.delete(event.key)
+        return newSet
+      })
+    }
+
+    window.addEventListener("keydown", handleKeyDown)
+    window.addEventListener("keyup", handleKeyUp)
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+      window.removeEventListener("keyup", handleKeyUp)
+    }
+  }, [])
+
+  return useMemo(() => keysPressed.has(key), [keysPressed, key])
+}
