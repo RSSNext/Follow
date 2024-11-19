64c891f30c27ff1c868a4ba8fb88adf2acda7dfd
lawvs <18554747+lawvs@users.noreply.github.com>
2024-11-20 01:34:02 +0800

    fix: enhance tray icon visual effect in Windows platform

--- apps/main/src/helper.ts
+++ apps/main/src/helper.ts
@@ -1,7 +1,7 @@
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 
-import { isMacOS } from "./env"
+import { isMacOS, isWindows } from "./env"
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url))
 const iconMap = {
@@ -9,5 +9,13 @@ const iconMap = {
   dev: path.join(__dirname, "../../static/icon-dev.png"),
 }
 export const getIconPath = () => iconMap[process.env.NODE_ENV === "development" ? "dev" : "prod"]
-export const getTrayIconPath = () =>
-  isMacOS ? path.join(__dirname, "../../resources/tray-icon.png") : getIconPath()
+export const getTrayIconPath = () => {
+  if (isMacOS) {
+    return path.join(__dirname, "../../resources/tray-icon.png")
+  }
+  if (isWindows) {
+    // https://www.electronjs.org/docs/latest/api/tray#:~:text=Windows,best%20visual%20effects.
+    return path.join(__dirname, "../../resources/icon.ico")
+  }
+  return getIconPath()
+}
