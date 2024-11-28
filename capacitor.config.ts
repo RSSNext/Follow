dfec94e618bc51f91e23dadc39f596c21b36c336
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-11-28 15:16:43 +0800

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,5 +1,22 @@
 import type { CapacitorConfig } from "@capacitor/cli"
 
+// <key>CFBundleURLTypes</key>
+// <array>
+//   <dict>
+//     <key>CFBundleURLName</key>
+//     <string>com.example.app</string>
+//     <key>CFBundleURLSchemes</key>
+//     <array>
+//       <string>follow</string>
+//     </array>
+//   </dict>
+// </array>
+// <key>WKAppBoundDomains</key>
+// <array>
+//   <string>app.follow.is</string>
+//   <string>api.follow.is</string>
+// </array>
+
 const config: CapacitorConfig = {
   appId: "com.example.app",
   appName: "Follow",
