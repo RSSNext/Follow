af1131ed196f94145ac0c0c83a2c35068a4eee85
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-12-12 09:56:21 +0800

    less rerender

--- apps/renderer/src/modules/action/action-setting.tsx
+++ apps/renderer/src/modules/action/action-setting.tsx
@@ -11,8 +11,26 @@ import { useActionsQuery } from "~/queries/actions"
 import { actionActions, useActions, useIsActionDataDirty } from "~/store/action"
 
 export const ActionSetting = () => {
-  const { t } = useTranslation("settings")
   const actionQuery = useActionsQuery()
+  const actionLength = useActions((actions) => actions.length)
+
+  if (actionQuery.isPending) {
+    return <LoadingWithIcon icon={<i className="i-mgc-magic-2-cute-re" />} size="large" />
+  }
+
+  return (
+    <div className="w-full max-w-4xl space-y-4">
+      {Array.from({ length: actionLength }).map((_action, actionIdx) => (
+        <ActionCard key={actionIdx} index={actionIdx} />
+      ))}
+      <ActionSettingOperations />
+    </div>
+  )
+}
+
+function ActionSettingOperations() {
+  const { t } = useTranslation("settings")
+
   const actionLength = useActions((actions) => actions.length)
   const isDirty = useIsActionDataDirty()
 
@@ -30,15 +48,8 @@ export const ActionSetting = () => {
     },
   })
 
-  if (actionQuery.isPending) {
-    return <LoadingWithIcon icon={<i className="i-mgc-magic-2-cute-re" />} size="large" />
-  }
-
   return (
-    <div className="w-full max-w-4xl space-y-4">
-      {Array.from({ length: actionLength }).map((_action, actionIdx) => (
-        <ActionCard key={actionIdx} index={actionIdx} />
-      ))}
+    <>
       <Button
         variant="outline"
         className="center w-full gap-1"
@@ -59,6 +70,6 @@ export const ActionSetting = () => {
           {t("actions.save")}
         </Button>
       </div>
-    </div>
+    </>
   )
 }
