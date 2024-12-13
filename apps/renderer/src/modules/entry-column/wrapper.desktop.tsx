577cae3905ce0d3cf43bd185689237f3f8a7c2cf
xiangzy1 <xiangzy8@gmail.com>
2024-12-13 18:08:53 +0800

    fix: feed column flickering

--- apps/renderer/src/modules/entry-column/wrapper.desktop.tsx
+++ apps/renderer/src/modules/entry-column/wrapper.desktop.tsx
@@ -3,6 +3,7 @@ import { views } from "@follow/constants"
 import { clsx } from "clsx"
 import { forwardRef } from "react"
 
+import { useReduceMotion } from "~/hooks/biz/useReduceMotion"
 import { useRouteParamsSelector } from "~/hooks/biz/useRouteParams"
 
 import type { EntryColumnWrapperProps } from "./wrapper.shared"
@@ -12,8 +13,10 @@ export const EntryColumnWrapper = forwardRef<HTMLDivElement, EntryColumnWrapperP
   ({ children, onScroll }, ref) => {
     const view = useRouteParamsSelector((state) => state.view)
 
+    const reduceMotion = useReduceMotion()
+
     return (
-      <div className={clsx(styles, animationStyles, "mt-2")}>
+      <div className={clsx(styles, !reduceMotion && animationStyles, "mt-2")}>
         <ScrollArea
           scrollbarClassName={clsx(!views[view].wideMode ? "w-[5px] p-0" : "", "z-[3]")}
           mask={false}
