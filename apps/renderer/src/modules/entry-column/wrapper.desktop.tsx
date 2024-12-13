36df9cda6334b4f4d000208ac85c2fde5a98d55f
xiangzy1 <xiangzy8@gmail.com>
2024-12-13 18:50:13 +0800

    fix: update

--- apps/renderer/src/modules/entry-column/wrapper.desktop.tsx
+++ apps/renderer/src/modules/entry-column/wrapper.desktop.tsx
@@ -3,7 +3,6 @@ import { views } from "@follow/constants"
 import { clsx } from "clsx"
 import { forwardRef } from "react"
 
-import { useReduceMotion } from "~/hooks/biz/useReduceMotion"
 import { useRouteParamsSelector } from "~/hooks/biz/useRouteParams"
 
 import type { EntryColumnWrapperProps } from "./wrapper.shared"
@@ -13,10 +12,8 @@ export const EntryColumnWrapper = forwardRef<HTMLDivElement, EntryColumnWrapperP
   ({ children, onScroll }, ref) => {
     const view = useRouteParamsSelector((state) => state.view)
 
-    const reduceMotion = useReduceMotion()
-
     return (
-      <div className={clsx(styles, !reduceMotion && animationStyles, "mt-2")}>
+      <div className={clsx(styles, animationStyles, "mt-2")}>
         <ScrollArea
           scrollbarClassName={clsx(!views[view].wideMode ? "w-[5px] p-0" : "", "z-[3]")}
           mask={false}
