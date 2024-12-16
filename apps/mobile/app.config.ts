508ad0dc8537dafb30caed32eee8aeeb8c57a559
lawvs <18554747+lawvs@users.noreply.github.com>
2024-12-17 02:46:08 +0800

    feat: add withFollowAssets plugin for managing asset resources

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -54,6 +54,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
         },
       },
     ],
+    [require("./scripts/withFollowAssets.js")],
   ],
   experiments: {
     typedRoutes: true,
