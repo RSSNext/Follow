f77e4817b06aa8845fcf709a3d1234e97f5915ae
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-12-16 14:26:45 +0800

    chore: update

--- apps/server/client/pages/(login)/register.tsx
+++ apps/server/client/pages/(login)/register.tsx
@@ -1,13 +1,6 @@
 import { Logo } from "@follow/components/icons/logo.jsx"
 import { Button } from "@follow/components/ui/button/index.jsx"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@follow/components/ui/card/index.jsx"
-import {
   Form,
   FormControl,
   FormField,
@@ -19,15 +12,18 @@ import { Input } from "@follow/components/ui/input/index.js"
 import { signUp } from "@follow/shared/auth"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { useTranslation } from "react-i18next"
+import { Trans, useTranslation } from "react-i18next"
+import { Link } from "react-router"
 import { toast } from "sonner"
 import { z } from "zod"
 
 export function Component() {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-10">
-      <Logo className="size-20" />
-      <RegisterForm />
+      <div className="max-w-sm space-y-6">
+        <Logo className="size-12" />
+        <RegisterForm />
+      </div>
     </div>
   )
 }
@@ -36,10 +32,11 @@ const formSchema = z
   .object({
     email: z.string().email(),
     password: z.string().min(8).max(128),
-    passwordConfirmation: z.string(),
+    confirmPassword: z.string(),
   })
-  .refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords do not match",
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
   })
 
 function onSubmit(values: z.infer<typeof formSchema>) {
@@ -67,62 +64,76 @@ function RegisterForm() {
     defaultValues: {
       email: "",
       password: "",
-      passwordConfirmation: "",
+      confirmPassword: "",
     },
   })
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{t("register.label")}</CardTitle>
-        <CardDescription>{t("register.description")}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("register.email")}</FormLabel>
-                  <FormControl>
-                    <Input type="email" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("register.password")}</FormLabel>
-                  <FormControl>
-                    <Input type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="passwordConfirmation"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("register.confirm_password")}</FormLabel>
-                  <FormControl>
-                    <Input type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">{t("register.submit")}</Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+    <div className="space-y-4">
+      <div className="space-y-2">
+        <h3 className="text-2xl font-semibold leading-none tracking-tight">
+          {t("register.label", { app_name: APP_NAME })}
+        </h3>
+        <p className="text-sm text-muted-foreground">
+          <Trans
+            ns="external"
+            i18nKey="register.note"
+            components={{
+              Login: (
+                <Link to="/login" className="text-primary hover:underline">
+                  {t("register.login")}
+                </Link>
+              ),
+            }}
+          />
+        </p>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("register.email")}</FormLabel>
+                <FormControl>
+                  <Input type="email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("register.password")}</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("register.confirm_password")}</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full">
+            {t("register.submit")}
+          </Button>
+        </form>
+      </Form>
+    </div>
   )
 }
