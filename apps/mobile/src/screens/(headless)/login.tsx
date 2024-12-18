bba3ab9f2dae34f8fa44f125f8f662677d3c944e
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-12-18 10:41:51 +0800

    feat: react-hook-form

--- apps/mobile/src/screens/(headless)/login.tsx
+++ apps/mobile/src/screens/(headless)/login.tsx
@@ -1,21 +1,54 @@
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Redirect } from "expo-router"
-import { useState } from "react"
+import * as React from "react"
+import type { Control } from "react-hook-form"
+import { useController, useForm } from "react-hook-form"
+import type { TextInputProps } from "react-native"
 import { Button, TextInput } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { z } from "zod"
 
 import { signIn, useAuthToken } from "@/src/lib/auth"
 
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().max(128),
+})
+
+type FormValue = z.infer<typeof formSchema>
+
+async function onSubmit(values: FormValue) {
+  await signIn.email({
+    email: values.email,
+    password: values.password,
+  })
+}
+
+function Input({
+  control,
+  name,
+  ...rest
+}: TextInputProps & {
+  control: Control<FormValue>
+  name: keyof FormValue
+}) {
+  const { field } = useController({
+    control,
+    name,
+  })
+  return <TextInput {...rest} value={field.value} onChangeText={field.onChange} />
+}
+
 export default function App() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
   const { data: token } = useAuthToken()
 
-  const handleLogin = async () => {
-    await signIn.email({
-      email,
-      password,
-    })
-  }
+  const { control, handleSubmit } = useForm<FormValue>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
 
   if (token) {
     return <Redirect href="/" />
@@ -23,9 +56,24 @@ export default function App() {
 
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <TextInput placeholder="Password" value={password} onChangeText={setPassword} />
-      <Button title="Login" onPress={handleLogin} />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="email-address"
+        autoComplete="email"
+        placeholder="Email"
+        control={control}
+        name="email"
+      />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        autoComplete="current-password"
+        placeholder="Password"
+        control={control}
+        name="password"
+      />
+      <Button title="Login" onPress={handleSubmit(onSubmit)} />
     </SafeAreaView>
   )
 }
