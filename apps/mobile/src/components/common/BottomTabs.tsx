437fa4bf7817527a7864a9ae5ca5aab63888ade0
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-12-18 08:49:22 +0800

    chore: remove unused code

--- apps/mobile/src/components/common/BottomTabs.tsx
+++
@@ -1,4 +0,0 @@
-import { createNativeBottomTabNavigator } from "@bottom-tabs/react-navigation"
-import { withLayoutContext } from "expo-router"
-
-export const Tabs = withLayoutContext(createNativeBottomTabNavigator().Navigator)
