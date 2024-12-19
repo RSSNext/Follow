a4eae6e3b1ba795b468a0cc461468d122b46f816
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-12-19 15:51:24 +0800

    chore: turn off typechecked rules in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import path from "node:path"
 
-import { defineConfig, GLOB_TS_SRC } from "eslint-config-hyoban"
+import { defineConfig } from "eslint-config-hyoban"
 
 import checkI18nJson from "./plugins/eslint/eslint-check-i18n-json.js"
 import noDebug from "./plugins/eslint/eslint-no-debug.js"
 import packageJsonExtend from "./plugins/eslint/eslint-package-json.js"
 import recursiveSort from "./plugins/eslint/eslint-recursive-sort.js"
-
-const isCI = process.env.CI === "true" || process.env.CI === "1"
 
 export default defineConfig(
   {
@@ -21,23 +19,6 @@ export default defineConfig(
       "resources/**",
     ],
     preferESM: false,
-    projectService: isCI
-      ? {
-          allowDefaultProject: ["apps/main/preload/index.d.ts"],
-          defaultProject: "tsconfig.json",
-        }
-      : undefined,
-    typeChecked: isCI ? "essential" : false,
-  },
-  isCI && {
-    files: GLOB_TS_SRC,
-    rules: {
-      "require-await": "off",
-      "@typescript-eslint/require-await": "warn",
-      "@typescript-eslint/await-thenable": "warn",
-      "@typescript-eslint/no-floating-promises": "off",
-      "@typescript-eslint/no-misused-promises": "off",
-    },
   },
   {
     settings: {
