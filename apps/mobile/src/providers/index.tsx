45e0afc371447293694cf3a3f46bf16c1267d4a8
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-12-19 15:48:33 +0800

    chore: add expo-drizzle-studio-plugin

--- apps/mobile/src/providers/index.tsx
+++ apps/mobile/src/providers/index.tsx
@@ -1,16 +1,19 @@
 import { ThemeProvider } from "@react-navigation/native"
 import { QueryClientProvider } from "@tanstack/react-query"
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin"
 import { useColorScheme } from "nativewind"
 import type { ReactNode } from "react"
 import { View } from "react-native"
 import { KeyboardProvider } from "react-native-keyboard-controller"
 
+import { sqlite } from "../database"
 import { queryClient } from "../lib/query-client"
 import { getCurrentColors } from "../theme/colors"
 import { DarkTheme, DefaultTheme } from "../theme/navigation"
 import { MigrationProvider } from "./migration"
 
 export const RootProviders = ({ children }: { children: ReactNode }) => {
+  useDrizzleStudio(sqlite)
   const { colorScheme } = useColorScheme()
 
   const currentThemeColors = getCurrentColors()!
