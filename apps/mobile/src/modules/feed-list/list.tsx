5dc7f8cd8e72cb2b207d948042908cec3e802d66
Innei <tukon479@gmail.com>
2024-12-19 22:32:11 +0800

    fix: enhance FeedIcon styling for dark mode compatibility
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/mobile/src/modules/feed-list/list.tsx
+++ apps/mobile/src/modules/feed-list/list.tsx
@@ -148,7 +148,7 @@ const SubscriptionItem = memo(({ id, className }: { id: string; className?: stri
         className,
       )}
     >
-      <View className="mr-2 overflow-hidden rounded-full">
+      <View className="mr-2 overflow-hidden rounded-full border border-transparent dark:border-tertiary-system-background">
         <FeedIcon feed={feed} />
       </View>
       <Text className="text-text">{subscription.title || feed.title}</Text>
