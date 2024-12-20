93e92d22ff490f4a1aa07849259f39246e3a0f3a
Innei <Innei@users.noreply.github.com>
2024-12-20 09:16:56 +0000

    chore: auto-fix linting and formatting issues

--- apps/mobile/src/modules/feed-list/ViewTab.tsx
+++ apps/mobile/src/modules/feed-list/ViewTab.tsx
@@ -22,7 +22,7 @@ export const ViewTab = () => {
           top: headerHeight - StyleSheet.hairlineWidth,
         },
       ]}
-      className="border-b border-system-fill/60"
+      className="border-system-fill/60 border-b"
     >
       <ScrollView
         className="border-tertiary-system-background"
