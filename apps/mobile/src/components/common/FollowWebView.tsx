eacffce1e3023b5f45f975fecbb72171a79e5597
lawvs <18554747+lawvs@users.noreply.github.com>
2024-12-24 03:09:45 +0800

    feat: implement deep linking support and refactor navigation handling in FollowWebView

--- apps/mobile/src/components/common/FollowWebView.tsx
+++ apps/mobile/src/components/common/FollowWebView.tsx
@@ -1,6 +1,8 @@
+import { callWebviewExpose } from "@follow/shared"
 import { parseSafeUrl, transformVideoUrl } from "@follow/utils"
+import * as Linking from "expo-linking"
 import type { RefObject } from "react"
-import { useCallback } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { Platform } from "react-native"
 import type { WebViewNavigation, WebViewProps } from "react-native-webview"
 import { WebView } from "react-native-webview"
@@ -20,12 +22,60 @@ interface FollowWebViewProps extends WebViewProps {
   customUrl?: string
 }
 
+const injectedJavaScript = [
+  `window.__RN__ = true`,
+  // TODO use more reliable way to detect the page is ready
+  `window.setTimeout(() => window.ReactNativeWebView.postMessage("ready"), 2000)`,
+].join(";")
+
 export const FollowWebView = ({
   webViewRef,
   customUrl,
   ...props
 }: { webViewRef: RefObject<WebView> } & FollowWebViewProps) => {
+  const { onNavigationStateChange } = useWebViewNavigation({ webViewRef })
+  const [ready, setReady] = useState(false)
+  useDeepLink({ webViewRef, ready })
+
+  return (
+    <WebView
+      ref={webViewRef}
+      source={{ uri: customUrl ?? presetUri }}
+      injectedJavaScriptBeforeContentLoaded={injectedJavaScript}
+      sharedCookiesEnabled
+      originWhitelist={["*"]}
+      allowUniversalAccessFromFileURLs
+      startInLoadingState
+      allowsBackForwardNavigationGestures
+      /* Open chrome://inspect/#devices , or Development menu on Safari to debug the WebView. https://github.com/react-native-webview/react-native-webview/blob/master/docs/Debugging.md#debugging-webview-contents */
+      webviewDebuggingEnabled={__DEV__}
+      containerStyle={{ width: "100%" }}
+      onNavigationStateChange={onNavigationStateChange}
+      // onLoad={() => setReady(false)}
+      // onLoadProgress={({ nativeEvent }) => setProgress(nativeEvent.progress)}
+      onError={(e) => {
+        console.error("WebView error:", e)
+      }}
+      onContentProcessDidTerminate={() => webViewRef.current?.reload()}
+      onMessage={(e) => {
+        const message = e.nativeEvent.data
+        if (message === "sign-out") {
+          signOut()
+          return
+        }
+        if (message === "ready") {
+          setReady(true)
+          return
+        }
+      }}
+      {...props}
+    />
+  )
+}
+
+const useWebViewNavigation = ({ webViewRef }: { webViewRef: RefObject<WebView> }) => {
   const openLink = useOpenLink()
+
   const onNavigationStateChange = useCallback(
     (newNavState: WebViewNavigation) => {
       const { url: urlStr } = newNavState
@@ -46,32 +96,63 @@ export const FollowWebView = ({
     [openLink, webViewRef],
   )
 
-  return (
-    <WebView
-      ref={webViewRef}
-      source={{ uri: customUrl ?? presetUri }}
-      injectedJavaScriptBeforeContentLoaded="window.__RN__ = true;"
-      sharedCookiesEnabled
-      originWhitelist={["*"]}
-      allowUniversalAccessFromFileURLs
-      startInLoadingState
-      allowsBackForwardNavigationGestures
-      /* Open chrome://inspect/#devices , or Development menu on Safari to debug the WebView. https://github.com/react-native-webview/react-native-webview/blob/master/docs/Debugging.md#debugging-webview-contents */
-      webviewDebuggingEnabled={__DEV__}
-      containerStyle={{ width: "100%" }}
-      onNavigationStateChange={onNavigationStateChange}
-      // onLoadProgress={({ nativeEvent }) => setProgress(nativeEvent.progress)}
-      onError={(e) => {
-        console.error("WebView error:", e)
-      }}
-      onContentProcessDidTerminate={() => webViewRef.current?.reload()}
-      onMessage={(e) => {
-        const message = e.nativeEvent.data
-        if (message === "sign-out") {
-          signOut()
-        }
-      }}
-      {...props}
-    />
+  return { onNavigationStateChange }
+}
+
+// We only need to handle deep link at the first time the app is opened
+let lastInitialUrl: string | null = null
+const useDeepLink = ({
+  webViewRef,
+  ready = true,
+}: {
+  webViewRef: RefObject<WebView>
+  ready?: boolean
+}) => {
+  const handleDeepLink = useCallback(
+    async (url: string) => {
+      const { queryParams } = Linking.parse(url)
+      if (!queryParams) {
+        console.error("Invalid URL! queryParams is not available", url)
+        return
+      }
+      const id = queryParams["id"] ?? undefined
+      const isList = queryParams["type"] === "list"
+      // const urlParam = queryParams["url"] ?? undefined
+      if (!id || typeof id !== "string") {
+        console.error("Invalid URL! id is not a string", url)
+        return
+      }
+      const injectJavaScript = webViewRef.current?.injectJavaScript
+      if (!injectJavaScript) {
+        console.error("injectJavaScript is not available")
+        return
+      }
+      callWebviewExpose(injectJavaScript).follow({ id, isList })
+    },
+    [webViewRef],
   )
+
+  // https://reactnative.dev/docs/linking#handling-deep-links
+  //
+  // The handler added with Linking.addEventListener() is only triggered when app is already open.
+  //
+  // When the app is not already open and the deep link triggered app launch,
+  // the URL can be obtained with Linking.getInitialURL().
+  useEffect(() => {
+    if (!ready) return
+
+    const getUrlAsync = async () => {
+      const initialUrl = await Linking.getInitialURL()
+      if (!initialUrl) return
+      if (initialUrl === lastInitialUrl) return
+      lastInitialUrl = initialUrl
+      handleDeepLink(initialUrl)
+    }
+    getUrlAsync()
+    // eslint-disable-next-line @eslint-react/web-api/no-leaked-event-listener
+    const deepLinkListener = Linking.addEventListener("url", (event: { url: string }) => {
+      handleDeepLink(event.url)
+    })
+    return () => deepLinkListener.remove()
+  }, [handleDeepLink, ready])
 }
