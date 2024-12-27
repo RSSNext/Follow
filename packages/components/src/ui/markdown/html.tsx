457cb1c188958bcb6ffcdc34b73881700b1744ad
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-12-27 15:55:59 +0800

    chore: prose style

--- packages/components/src/ui/markdown/html.tsx
+++ packages/components/src/ui/markdown/html.tsx
@@ -3,5 +3,9 @@ import { parseHtml } from "./parse-html"
 export function Html({ content }: { content: string }) {
   const res = parseHtml(content)
 
-  return <article>{res.toContent()}</article>
+  return (
+    <article className="prose !max-w-full dark:prose-invert prose-h1:text-[1.6em] prose-h1:font-bold">
+      {res.toContent()}
+    </article>
+  )
 }
