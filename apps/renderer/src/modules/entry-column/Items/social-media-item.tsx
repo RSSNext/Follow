51065f484e0f1dd515369fc398f019fc8af71ccf
Innei <tukon479@gmail.com>
2025-01-09 20:22:49 +0800

    style: update background color of ActionBar in SocialMediaItem component in light mode
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/renderer/src/modules/entry-column/Items/social-media-item.tsx
+++ apps/renderer/src/modules/entry-column/Items/social-media-item.tsx
@@ -118,7 +118,7 @@ export const SocialMediaItem: EntryListItemFC = ({ entryId, entryPreview, transl
       </div>
 
       {showAction && !isMobile && (
-        <div className="absolute right-1 top-0 -translate-y-1/2 rounded-lg border border-gray-200 bg-neutral-900 p-1 shadow-sm backdrop-blur-sm dark:border-neutral-900">
+        <div className="absolute right-1 top-0 -translate-y-1/2 rounded-lg border border-gray-200 bg-white/90 p-1 shadow-sm backdrop-blur-sm dark:border-neutral-900 dark:bg-neutral-900">
           <ActionBar entryId={entryId} />
         </div>
       )}
