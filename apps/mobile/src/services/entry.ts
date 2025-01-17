6b59d6485629e58a47a4063c51bfd336077c4c26
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-01-17 17:26:34 +0800

    use conflictUpdateAllExcept

--- apps/mobile/src/services/entry.ts
+++ apps/mobile/src/services/entry.ts
@@ -1,4 +1,4 @@
-import { eq, sql } from "drizzle-orm"
+import { eq } from "drizzle-orm"
 
 import { db } from "../database"
 import { entriesTable } from "../database/schemas"
@@ -6,6 +6,7 @@ import type { EntrySchema } from "../database/schemas/types"
 import { dbStoreMorph } from "../morph/db-store"
 import { entryActions } from "../store/entry/store"
 import type { Hydratable, Resetable } from "./internal/base"
+import { conflictUpdateAllExcept } from "./internal/utils"
 
 class EntryServiceStatic implements Hydratable, Resetable {
   async reset() {
@@ -19,26 +20,7 @@ class EntryServiceStatic implements Hydratable, Resetable {
       .values(entries)
       .onConflictDoUpdate({
         target: [entriesTable.id],
-        set: {
-          title: sql`excluded.title`,
-          url: sql`excluded.url`,
-          content: sql`excluded.content`,
-          description: sql`excluded.description`,
-          guid: sql`excluded.guid`,
-          author: sql`excluded.author`,
-          authorUrl: sql`excluded.author_url`,
-          authorAvatar: sql`excluded.author_avatar`,
-          insertedAt: sql`excluded.inserted_at`,
-          publishedAt: sql`excluded.published_at`,
-          media: sql`excluded.media`,
-          categories: sql`excluded.categories`,
-          attachments: sql`excluded.attachments`,
-          extra: sql`excluded.extra`,
-          language: sql`excluded.language`,
-          feedId: sql`excluded.feed_id`,
-          inboxHandle: sql`excluded.inbox_handle`,
-          read: sql`excluded.read`,
-        },
+        set: conflictUpdateAllExcept(entriesTable, ["id"]),
       })
   }
 
