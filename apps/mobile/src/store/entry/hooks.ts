45ba640b2b386bc1b23c239e277510b1da38520c
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-01-17 20:06:14 +0800

    sort

--- apps/mobile/src/store/entry/hooks.ts
+++ apps/mobile/src/store/entry/hooks.ts
@@ -1,12 +1,15 @@
 import type { FeedViewType } from "@follow/constants"
 import { useQuery } from "@tanstack/react-query"
+import { useCallback } from "react"
 
+import { getEntry } from "./getter"
 import { entrySyncServices, useEntryStore } from "./store"
 import type { FetchEntriesProps } from "./types"
 
 export const usePrefetchEntries = (props: FetchEntriesProps) => {
+  const { feedId, inboxId, listId, view, read, limit, pageParam, isArchived } = props
   return useQuery({
-    queryKey: ["entries", props],
+    queryKey: ["entries", feedId, inboxId, listId, view, read, limit, pageParam, isArchived],
     queryFn: () => entrySyncServices.fetchEntries(props),
   })
 }
@@ -15,18 +18,57 @@ export const useEntry = (id: string) => {
   return useEntryStore((state) => state.data[id])
 }
 
+function sortEntryIdsByPublishDate(a: string, b: string) {
+  const entryA = getEntry(a)
+  const entryB = getEntry(b)
+  if (!entryA || !entryB) return 0
+  return entryB.publishedAt.getTime() - entryA.publishedAt.getTime()
+}
+
 export const useEntryIdsByView = (view: FeedViewType) => {
-  return useEntryStore((state) => state.entryIdByView[view])
+  return useEntryStore(
+    useCallback(
+      (state) => {
+        const ids = state.entryIdByView[view]
+        return Array.from(ids).sort((a, b) => sortEntryIdsByPublishDate(a, b))
+      },
+      [view],
+    ),
+  )
 }
 
 export const useEntryIdsByFeedId = (feedId: string) => {
-  return useEntryStore((state) => state.entryIdByFeed[feedId])
+  return useEntryStore(
+    useCallback(
+      (state) => {
+        const ids = state.entryIdByFeed[feedId]
+        return Array.from(ids).sort((a, b) => sortEntryIdsByPublishDate(a, b))
+      },
+      [feedId],
+    ),
+  )
 }
 
 export const useEntryIdsByInboxId = (inboxId: string) => {
-  return useEntryStore((state) => state.entryIdByInbox[inboxId])
+  return useEntryStore(
+    useCallback(
+      (state) => {
+        const ids = state.entryIdByInbox[inboxId]
+        return Array.from(ids).sort((a, b) => sortEntryIdsByPublishDate(a, b))
+      },
+      [inboxId],
+    ),
+  )
 }
 
 export const useEntryIdsByCategory = (category: string) => {
-  return useEntryStore((state) => state.entryIdByCategory[category])
+  return useEntryStore(
+    useCallback(
+      (state) => {
+        const ids = state.entryIdByCategory[category]
+        return Array.from(ids).sort((a, b) => sortEntryIdsByPublishDate(a, b))
+      },
+      [category],
+    ),
+  )
 }
