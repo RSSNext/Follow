f947bd1d1ec2d91e05264c77c5c03f9de5fec6fb
Innei <tukon479@gmail.com>
2025-01-18 16:59:29 +0800

    feat: integrate react-native-uikit-colors for improved theming and UI consistency
    - Added `react-native-uikit-colors` package to enhance color management.
    - Updated `tailwind.config.ts` to utilize `withUIKit` for better color integration.
    - Refactored color handling in `colors.ts` and `utils.ts` to leverage the new package.
    - Adjusted components to use safe area insets and improved layout in `rsshub-form.tsx`.
    - Enhanced `MarkdownWeb` component with custom styles for better rendering.
    
    This update streamlines color management and improves the overall UI experience.
    
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/mobile/src/components/ui/typography/MarkdownWeb.tsx
+++ apps/mobile/src/components/ui/typography/MarkdownWeb.tsx
@@ -20,6 +20,16 @@ const MarkdownWeb: WebComponent<{ value: string; style?: React.CSSProperties }> 
       className={cn("text-text prose min-w-0", isDarkMode ? "prose-invert" : "prose")}
       style={style}
     >
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+        body, html {
+            height: 100%;
+            overflow: hidden;
+          }
+        `,
+        }}
+      />
       {useMemo(() => parseMarkdown(value).content, [value])}
     </div>
   )
