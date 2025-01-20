e4b2568e29d2df1170b45dd47c14aa68a3538941
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-01-20 11:05:51 +0800

    chore: update

--- apps/mobile/src/components/common/AnimatedComponents.tsx
+++ apps/mobile/src/components/common/AnimatedComponents.tsx
@@ -2,11 +2,11 @@ import { Image as ExpoImage } from "expo-image"
 import { Animated, FlatList, Pressable, ScrollView, TouchableOpacity } from "react-native"
 import Reanimated from "react-native-reanimated"
 
-export const ReAnimatedExpoImage = ReAnimated.createAnimatedComponent(ExpoImage)
 export const AnimatedScrollView = Animated.createAnimatedComponent(ScrollView)
 export const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
 export const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity)
 
+export const ReAnimatedExpoImage = Reanimated.createAnimatedComponent(ExpoImage)
 export const ReAnimatedPressable = Reanimated.createAnimatedComponent(Pressable)
 export const ReAnimatedScrollView = Reanimated.createAnimatedComponent(ScrollView)
 export const ReAnimatedTouchableOpacity = Reanimated.createAnimatedComponent(TouchableOpacity)
