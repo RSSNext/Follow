0c29f278e6ea42d36f6f9bc22ea368144e7361dc
DIYgod <i@diygod.me>
2025-01-22 19:24:51 +0800

    feat: view selector

--- apps/mobile/src/screens/(stack)/(tabs)/index.tsx
+++ apps/mobile/src/screens/(stack)/(tabs)/index.tsx
@@ -1,5 +1,11 @@
 import { EntryList } from "@/src/modules/entry-list/entry-list"
+import { ViewSelector } from "@/src/modules/feed-drawer/view-selector"
 
 export default function Index() {
-  return <EntryList />
+  return (
+    <>
+      <ViewSelector />
+      <EntryList />
+    </>
+  )
 }
