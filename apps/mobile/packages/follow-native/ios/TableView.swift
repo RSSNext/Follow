9abd5a488a139ae14b162256b08cf203e8125e48
Innei <tukon479@gmail.com>
2025-01-29 01:44:26 +0800

    test uikit
    Signed-off-by: Innei <tukon479@gmail.com>

---
+++ apps/mobile/packages/follow-native/ios/TableView.swift
@@ -0,0 +1,8 @@
+//
+//  TableView.swift
+//  FollowNative
+//
+//  Created by Innei on 2025/1/29.
+//
+
+import Foundation
---
+++ apps/mobile/packages/follow-native/ios/TableViewModule.swift
@@ -0,0 +1,8 @@
+//
+//  TableViewModule.swift
+//  FollowNative
+//
+//  Created by Innei on 2025/1/29.
+//
+
+import Foundation
