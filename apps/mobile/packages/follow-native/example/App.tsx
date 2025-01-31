3912d8c897259b761ab5c89f9fd07ea8e7bb4af6
Innei <tukon479@gmail.com>
2025-02-01 01:14:48 +0800

    refactor(shared-webview): Simplify WebView height management
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/mobile/packages/follow-native/example/App.tsx
+++ apps/mobile/packages/follow-native/example/App.tsx
@@ -1,20 +1,11 @@
 import { SharedWebView, SharedWebViewModule } from "follow-native"
-import { useState } from "react"
 import { ScrollView, View } from "react-native"
 
 SharedWebViewModule.preload("https://follow.is")
 export default function App() {
-  const [contentHeight, setContentHeight] = useState(0)
-
   return (
     <ScrollView>
-      <View style={{ height: contentHeight }}>
-        <SharedWebView
-          onContentHeightChange={(e) => {
-            setContentHeight(e.nativeEvent.height)
-          }}
-        />
-      </View>
+      <SharedWebView />
 
       <View style={{ height: 1000, backgroundColor: "red" }} />
     </ScrollView>
--- apps/mobile/packages/follow-native/src/SharedWebView.tsx
+++ apps/mobile/packages/follow-native/src/SharedWebView.tsx
@@ -1,8 +1,22 @@
 import { requireNativeView } from "expo"
 import type * as React from "react"
+import { useState } from "react"
+import { View } from "react-native"
 
 const NativeView: React.ComponentType<{
   onContentHeightChange?: (e: { nativeEvent: { height: number } }) => void
 }> = requireNativeView("FOSharedWebView")
 
-export default NativeView
+export default function SharedWebView() {
+  const [contentHeight, setContentHeight] = useState(0)
+
+  return (
+    <View style={{ height: contentHeight }}>
+      <NativeView
+        onContentHeightChange={(e) => {
+          setContentHeight(e.nativeEvent.height)
+        }}
+      />
+    </View>
+  )
+}
