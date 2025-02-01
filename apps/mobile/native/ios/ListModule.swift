37c821d674ef9515b4d08735fdd0643bb05ef60b
Innei <tukon479@gmail.com>
2025-02-02 01:50:38 +0800

    cleanup
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/mobile/native/ios/ListModule.swift
+++
@@ -1,8 +0,0 @@
-import ExpoModulesCore
-
-public class ListModule: Module {
-  public func definition() -> ModuleDefinition {
-    Name("FOListView")
-    View(FOList.self)
-  }
-}
--- apps/mobile/native/src/TableView.tsx
+++
@@ -1,8 +0,0 @@
-import { requireNativeView } from "expo"
-import * as React from "react"
-
-const NativeView: React.ComponentType = requireNativeView("FOTableView")
-
-export default function TableView() {
-  return <NativeView />
-}
