816b1efdb0ba38d8526b60bfade7cc059bee3f72
Innei <tukon479@gmail.com>
2025-02-02 23:35:37 +0800

    fix: export
    Signed-off-by: Innei <tukon479@gmail.com>

--- apps/mobile/native/src/index.ts
+++ apps/mobile/native/src/index.ts
@@ -1,9 +1,7 @@
 import type { NativeModule } from "expo"
 import { requireNativeModule } from "expo-modules-core"
 
-export { default as ListView } from "./ListView"
 export { default as SharedWebView } from "./SharedWebView"
-export { default as TableView } from "./TableView"
 
 declare class ISharedWebViewModule extends NativeModule {
   preload(url: string): void
