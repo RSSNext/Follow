67ee27c8831b4a3351f9d6e21ee018807ab0f719
lawvs <18554747+lawvs@users.noreply.github.com>
2025-02-13 08:34:12 +0000

    chore: auto-fix linting and formatting issues

--- apps/mobile/src/modules/entry-list/entry-list.tsx
+++ apps/mobile/src/modules/entry-list/entry-list.tsx
@@ -1,9 +1,7 @@
 import { FeedViewType } from "@follow/constants"
 
 import { EntryListContentGrid } from "@/src/modules/entry-list/entry-list-gird"
-import {
-  useSelectedView
-} from "@/src/modules/screen/atoms"
+import { useSelectedView } from "@/src/modules/screen/atoms"
 import { TimelineSelectorHeader } from "@/src/modules/screen/timeline-selector-header"
 
 import { EntryListContentArticle } from "./entry-list-article"
