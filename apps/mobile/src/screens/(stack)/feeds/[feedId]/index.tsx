62d8b0042201c8af9c75b11eb0145b891b0c3613
lawvs <18554747+lawvs@users.noreply.github.com>
2025-02-17 17:46:43 +0800

    feat(mobile): implement collection entry list

--- apps/mobile/src/screens/(stack)/feeds/[feedId]/index.tsx
+++ apps/mobile/src/screens/(stack)/feeds/[feedId]/index.tsx
@@ -4,20 +4,30 @@ import { useMemo } from "react"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 import { EntryListSelector } from "@/src/modules/entry-list/EntryListSelector"
-import { EntryListContext } from "@/src/modules/screen/atoms"
+import { EntryListContext, useSelectedView } from "@/src/modules/screen/atoms"
+import { useCollectionEntryList } from "@/src/store/collection/hooks"
 import { useEntryIdsByCategory, useEntryIdsByFeedId } from "@/src/store/entry/hooks"
+import { FEED_COLLECTION_LIST } from "@/src/store/entry/utils"
 
 export default function Feed() {
+  const insets = useSafeAreaInsets()
   const { feedId: feedIdOrCategory } = useLocalSearchParams()
   const entryIdsByFeedId = useEntryIdsByFeedId(feedIdOrCategory as string)
   const entryIdsByCategory = useEntryIdsByCategory(feedIdOrCategory as string)
-  const insets = useSafeAreaInsets()
+  const isCollection = feedIdOrCategory === FEED_COLLECTION_LIST
+  const view = useSelectedView()
+  const collectionEntryIds = useCollectionEntryList(view ?? 0)
+
+  const entryIds = isCollection
+    ? collectionEntryIds
+    : entryIdsByFeedId.length > 0
+      ? entryIdsByFeedId
+      : entryIdsByCategory
+
   return (
     <EntryListContext.Provider value={useMemo(() => ({ type: "feed" }), [])}>
       <BottomTabBarHeightContext.Provider value={insets.bottom}>
-        <EntryListSelector
-          entryIds={entryIdsByFeedId.length > 0 ? entryIdsByFeedId : entryIdsByCategory}
-        />
+        <EntryListSelector entryIds={entryIds} />
       </BottomTabBarHeightContext.Provider>
     </EntryListContext.Provider>
   )
--- apps/mobile/src/store/collection/hooks.ts
+++ apps/mobile/src/store/collection/hooks.ts
@@ -1,13 +1,40 @@
+import type { FeedViewType } from "@follow/constants"
+import { useCallback } from "react"
+
 import { useCollectionStore } from "./store"
 
 export const useCollectionEntry = (entryId: string) => {
-  return useCollectionStore((state) => {
-    return state.collections[entryId]
-  })
+  return useCollectionStore(
+    useCallback(
+      (state) => {
+        return state.collections[entryId]
+      },
+      [entryId],
+    ),
+  )
 }
 
 export const useIsEntryStarred = (entryId: string) => {
-  return useCollectionStore((state) => {
-    return !!state.collections[entryId]
-  })
+  return useCollectionStore(
+    useCallback(
+      (state) => {
+        return !!state.collections[entryId]
+      },
+      [entryId],
+    ),
+  )
+}
+
+export const useCollectionEntryList = (view: FeedViewType) => {
+  return useCollectionStore(
+    useCallback(
+      (state) => {
+        return Object.values(state.collections)
+          .filter((collection) => collection.view === view)
+          .sort((a, b) => (new Date(a.createdAt ?? 0) > new Date(b.createdAt ?? 0) ? -1 : 1))
+          .map((i) => i.entryId)
+      },
+      [view],
+    ),
+  )
 }
