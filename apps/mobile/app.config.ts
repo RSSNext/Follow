ac64569da087bf52fca256c79180628f9914e160
DIYgod <i@diygod.me>
2025-02-19 16:08:16 +0800

    chore: fix test

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -26,7 +26,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 
   name: "Follow",
   slug: "follow",
-  version: process.env.NODE_ENV === "development" ? "dev" : PKG.version,
+  version: PKG.version,
   orientation: "portrait",
   icon: iconPath,
   scheme: "follow",
