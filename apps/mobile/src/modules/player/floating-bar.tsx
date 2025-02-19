09a86d0f8a35bbd447a066d59f07ad731d13d310
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-02-19 11:50:45 +0800

    blur view

--- apps/mobile/src/modules/player/floating-bar.tsx
+++ apps/mobile/src/modules/player/floating-bar.tsx
@@ -3,6 +3,7 @@ import { Image } from "expo-image"
 import { Text, View } from "react-native"
 import ReAnimated, { SlideInDown, SlideOutDown } from "react-native-reanimated"
 
+import { ThemedBlurView } from "@/src/components/common/ThemedBlurView"
 import { useActiveTrack } from "@/src/lib/player"
 
 import { PlayPauseButton, SeekButton } from "./control"
@@ -15,23 +16,22 @@ export function FloatingBar({ className }: { className?: string }) {
 
   return (
     <ReAnimated.View
-      className={cn(
-        "mx-4 flex flex-row items-center gap-4 rounded-2xl bg-white p-2 shadow-md",
-        className,
-      )}
+      className={cn("mx-4 shadow-md", className)}
       entering={SlideInDown}
       exiting={SlideOutDown}
     >
-      <Image source={activeTrack.artwork} className="size-12 rounded-lg" />
-      <View className="flex-1 overflow-hidden">
-        <Text className="text-lg font-semibold" numberOfLines={1}>
-          {activeTrack.title}
-        </Text>
-      </View>
-      <View className="mr-2 flex flex-row gap-4">
-        <PlayPauseButton />
-        <SeekButton />
-      </View>
+      <ThemedBlurView className="flex flex-row items-center gap-4 overflow-hidden rounded-2xl p-2">
+        <Image source={activeTrack.artwork} className="size-12 rounded-lg" />
+        <View className="flex-1 overflow-hidden">
+          <Text className="text-lg font-semibold" numberOfLines={1}>
+            {activeTrack.title}
+          </Text>
+        </View>
+        <View className="mr-2 flex flex-row gap-4">
+          <PlayPauseButton />
+          <SeekButton />
+        </View>
+      </ThemedBlurView>
     </ReAnimated.View>
   )
 }
