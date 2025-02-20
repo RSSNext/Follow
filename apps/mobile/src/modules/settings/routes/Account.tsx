79b47030260e7667e23e725b2337922139b6ae6b
DIYgod <i@diygod.me>
2025-02-20 10:49:28 +0800

    feat(mobile): deletion button

---
+++ apps/mobile/src/modules/settings/routes/Account.tsx
@@ -0,0 +1,40 @@
+import * as FileSystem from "expo-file-system"
+import { Text, TouchableOpacity, View } from "react-native"
+
+import {
+  NavigationBlurEffectHeader,
+  SafeNavigationScrollView,
+} from "@/src/components/common/SafeNavigationScrollView"
+import {
+  GroupedInsetListBaseCell,
+  GroupedInsetListCard,
+  GroupedInsetListSectionHeader,
+} from "@/src/components/ui/grouped/GroupedList"
+import { getDbPath } from "@/src/database"
+import { signOut } from "@/src/lib/auth"
+
+export const AccountScreen = () => {
+  return (
+    <SafeNavigationScrollView className="bg-system-grouped-background">
+      <NavigationBlurEffectHeader title="Account" />
+      {/* Language */}
+      <View className="mt-6">
+        <GroupedInsetListSectionHeader label="Danger Zone" />
+        <GroupedInsetListCard>
+          <GroupedInsetListBaseCell>
+            <TouchableOpacity
+              onPress={async () => {
+                await signOut()
+                const dbPath = getDbPath()
+                await FileSystem.deleteAsync(dbPath)
+                await expo.reloadAppAsync("User sign out")
+              }}
+            >
+              <Text className="text-label">Delete account</Text>
+            </TouchableOpacity>
+          </GroupedInsetListBaseCell>
+        </GroupedInsetListCard>
+      </View>
+    </SafeNavigationScrollView>
+  )
+}
