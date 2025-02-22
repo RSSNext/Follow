4a0b8cbccc35d05a9a740a654adf6eba10c9bc6e
ghostendsky <9551552+ghostendsky@users.noreply.github.com>
2025-02-22 14:21:35 +0800

    update

--- apps/main/src/@types/resources.ts
+++ apps/main/src/@types/resources.ts
@@ -1,21 +1,21 @@
 import en from "@locales/native/en.json"
 import ja from "@locales/native/ja.json"
-import zh_cn from "@locales/native/zh-CN.json"
-import zh_hk from "@locales/native/zh-HK.json"
-import zh_tw from "@locales/native/zh-TW.json"
+import zhCn from "@locales/native/zh-CN.json"
+import zhHk from "@locales/native/zh-HK.json"
+import zhTw from "@locales/native/zh-TW.json"
 
 export const resources = {
   en: {
     native: en,
   },
   "zh-CN": {
-    native: zh_cn,
+    native: zhCn,
   },
   "zh-HK": {
-    native: zh_hk,
+    native: zhHk,
   },
   "zh-TW": {
-    native: zh_tw,
+    native: zhTw,
   },
   ja: {
     native: ja,
