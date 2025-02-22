de692fa32ad140bc7681b5abdc96fba963f93939
ghostendsky <9551552+ghostendsky@users.noreply.github.com>
2025-02-22 14:11:16 +0800

    feat(locales): add support for zh_HK and zh_TW context menu locales

--- apps/main/src/@types/resources.ts
+++ apps/main/src/@types/resources.ts
@@ -1,13 +1,21 @@
 import en from "@locales/native/en.json"
 import ja from "@locales/native/ja.json"
-import zh from "@locales/native/zh-CN.json"
+import zh_cn from "@locales/native/zh-CN.json"
+import zh_hk from "@locales/native/zh-HK.json"
+import zh_tw from "@locales/native/zh-TW.json"
 
 export const resources = {
   en: {
     native: en,
   },
-  zh: {
-    native: zh,
+  "zh-CN": {
+    native: zh_cn,
+  },
+  "zh-HK": {
+    native: zh_hk,
+  },
+  "zh-TW": {
+    native: zh_tw,
   },
   ja: {
     native: ja,
