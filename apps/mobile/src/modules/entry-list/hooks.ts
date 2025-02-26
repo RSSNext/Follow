4f2c6afdcdb729fc1e197b666561363da97fda57
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-02-26 22:19:03 +0800

    fix(mobile): do not mark as reading when scrolling up

--- apps/mobile/src/modules/entry-list/hooks.ts
+++ apps/mobile/src/modules/entry-list/hooks.ts
@@ -1,5 +1,6 @@
 import type ViewToken from "@shopify/flash-list/dist/viewability/ViewToken"
-import { useCallback, useState } from "react"
+import { useMemo, useRef, useState } from "react"
+import type { NativeScrollEvent, NativeSyntheticEvent } from "react-native"
 
 import { useGeneralSettingKey } from "@/src/atoms/settings/general"
 import { debouncedFetchEntryContentByStream } from "@/src/store/entry/store"
@@ -12,33 +13,46 @@ export function useOnViewableItemsChanged({
 }: {
   isLoading?: boolean
   idExtractor?: (item: ViewToken) => string
-} = {}): (info: { viewableItems: ViewToken[]; changed: ViewToken[] }) => void {
+} = {}): {
+  onViewableItemsChanged: (info: { viewableItems: ViewToken[]; changed: ViewToken[] }) => void
+  onScroll?: (e: NativeSyntheticEvent<NativeScrollEvent>) => void
+} {
+  const orientation = useRef<"down" | "up">("down")
+  const lastOffset = useRef(0)
+
   const markAsReadWhenScrolling = useGeneralSettingKey("scrollMarkUnread")
   const markAsReadWhenRendering = useGeneralSettingKey("renderMarkUnread")
 
   const [stableIdExtractor] = useState(() => idExtractor)
 
-  return useCallback(
-    ({ viewableItems, changed }) => {
-      if (isLoading) return
+  return useMemo(() => {
+    return {
+      onViewableItemsChanged: ({ viewableItems, changed }) => {
+        if (isLoading) return
 
-      debouncedFetchEntryContentByStream(viewableItems.map((item) => stableIdExtractor(item)))
-      if (markAsReadWhenScrolling) {
-        changed
-          .filter((item) => !item.isViewable)
-          .forEach((item) => {
-            unreadSyncService.markEntryAsRead(stableIdExtractor(item))
-          })
-      }
+        debouncedFetchEntryContentByStream(viewableItems.map((item) => stableIdExtractor(item)))
+        if (markAsReadWhenScrolling && orientation.current === "down") {
+          changed
+            .filter((item) => !item.isViewable)
+            .forEach((item) => {
+              unreadSyncService.markEntryAsRead(stableIdExtractor(item))
+            })
+        }
 
-      if (markAsReadWhenRendering) {
-        viewableItems
-          .filter((item) => item.isViewable)
-          .forEach((item) => {
-            unreadSyncService.markEntryAsRead(stableIdExtractor(item))
-          })
-      }
-    },
-    [markAsReadWhenRendering, markAsReadWhenScrolling, stableIdExtractor, isLoading],
-  )
+        if (markAsReadWhenRendering && orientation.current === "down") {
+          viewableItems
+            .filter((item) => item.isViewable)
+            .forEach((item) => {
+              unreadSyncService.markEntryAsRead(stableIdExtractor(item))
+            })
+        }
+      },
+      onScroll: (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+        const currentOffset = e.nativeEvent.contentOffset.y
+        const currentOrientation = currentOffset > lastOffset.current ? "down" : "up"
+        orientation.current = currentOrientation
+        lastOffset.current = currentOffset
+      },
+    }
+  }, [isLoading, markAsReadWhenRendering, markAsReadWhenScrolling, orientation, stableIdExtractor])
 }
