78b66c3567d7df11a0e33906d2395b308f26054e
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-02-28 16:26:42 +0800

    fix(mobile): onViewableItemsChanged is not reactive

--- apps/mobile/src/modules/entry-list/hooks.ts
+++ apps/mobile/src/modules/entry-list/hooks.ts
@@ -1,5 +1,5 @@
 import type ViewToken from "@shopify/flash-list/dist/viewability/ViewToken"
-import { useMemo, useRef, useState } from "react"
+import { useCallback, useInsertionEffect, useMemo, useRef, useState } from "react"
 import type { NativeScrollEvent, NativeSyntheticEvent } from "react-native"
 
 import { useGeneralSettingKey } from "@/src/atoms/settings/general"
@@ -9,14 +9,9 @@ import { unreadSyncService } from "@/src/store/unread/store"
 const defaultIdExtractor = (item: ViewToken) => item.key
 export function useOnViewableItemsChanged({
   idExtractor = defaultIdExtractor,
-  disabled,
 }: {
-  disabled?: boolean
   idExtractor?: (item: ViewToken) => string
-} = {}): {
-  onViewableItemsChanged: (info: { viewableItems: ViewToken[]; changed: ViewToken[] }) => void
-  onScroll?: (e: NativeSyntheticEvent<NativeScrollEvent>) => void
-} {
+} = {}) {
   const orientation = useRef<"down" | "up">("down")
   const lastOffset = useRef(0)
 
@@ -25,32 +20,47 @@ export function useOnViewableItemsChanged({
 
   const [stableIdExtractor] = useState(() => idExtractor)
 
-  return useMemo(() => {
-    return {
-      onViewableItemsChanged: ({ viewableItems, changed }) => {
-        if (disabled) return
+  const onViewableItemsChanged: (info: {
+    viewableItems: ViewToken[]
+    changed: ViewToken[]
+  }) => void = useNonReactiveCallback(({ viewableItems, changed }) => {
+    debouncedFetchEntryContentByStream(viewableItems.map((item) => stableIdExtractor(item)))
 
-        debouncedFetchEntryContentByStream(viewableItems.map((item) => stableIdExtractor(item)))
-        if (markAsReadWhenScrolling && orientation.current === "down") {
-          changed
-            .filter((item) => !item.isViewable)
-            .forEach((item) => {
-              unreadSyncService.markEntryAsRead(stableIdExtractor(item))
-            })
-        }
-
-        if (markAsReadWhenRendering && orientation.current === "down") {
-          viewableItems.forEach((item) => {
-            unreadSyncService.markEntryAsRead(stableIdExtractor(item))
-          })
-        }
-      },
-      onScroll: (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-        const currentOffset = e.nativeEvent.contentOffset.y
-        const currentOrientation = currentOffset > lastOffset.current ? "down" : "up"
-        orientation.current = currentOrientation
-        lastOffset.current = currentOffset
-      },
+    if (markAsReadWhenScrolling && orientation.current === "down") {
+      changed
+        .filter((item) => !item.isViewable)
+        .forEach((item) => {
+          unreadSyncService.markEntryAsRead(stableIdExtractor(item))
+        })
     }
-  }, [disabled, markAsReadWhenRendering, markAsReadWhenScrolling, orientation, stableIdExtractor])
+
+    if (markAsReadWhenRendering && orientation.current === "down") {
+      viewableItems.forEach((item) => {
+        unreadSyncService.markEntryAsRead(stableIdExtractor(item))
+      })
+    }
+  })
+
+  const onScroll = useCallback((e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const currentOffset = e.nativeEvent.contentOffset.y
+    const currentOrientation = currentOffset > lastOffset.current ? "down" : "up"
+    orientation.current = currentOrientation
+    lastOffset.current = currentOffset
+  }, [])
+
+  return useMemo(() => ({ onViewableItemsChanged, onScroll }), [onScroll, onViewableItemsChanged])
+}
+
+function useNonReactiveCallback<T extends (...args: any[]) => any>(fn: T): T {
+  const ref = useRef(fn)
+  useInsertionEffect(() => {
+    ref.current = fn
+  }, [fn])
+  return useCallback(
+    (...args: any) => {
+      const latestFn = ref.current
+      return latestFn(...args)
+    },
+    [ref],
+  ) as unknown as T
 }
