31a0568d293d8e3d01d1d726e6161171f737fdb4
DIYgod <i@diygod.me>
2025-03-10 16:41:45 +0800

    feat(mobile): limited use token

--- apps/mobile/src/lib/token.ts
+++ apps/mobile/src/lib/token.ts
@@ -1,12 +1,12 @@
 import { getApp } from "@react-native-firebase/app"
-import getAppCheck, { getToken } from "@react-native-firebase/app-check"
+import getAppCheck, { getLimitedUseToken } from "@react-native-firebase/app-check"
 
 export async function getTokenHeaders() {
   const app = getApp()
   const appCheck = getAppCheck(app)
   let token = ""
   try {
-    const appCheckToken = await getToken(appCheck, false)
+    const appCheckToken = await getLimitedUseToken(appCheck)
     token = appCheckToken.token
   } catch (error) {
     console.error("error", error)
