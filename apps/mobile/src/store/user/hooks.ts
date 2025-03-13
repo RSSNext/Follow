48c4abe645c086946950f9850416d0192af13bf1
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2025-03-13 16:05:56 +0800

    feat(mobile): show onboarding check

--- apps/mobile/src/store/user/hooks.ts
+++ apps/mobile/src/store/user/hooks.ts
@@ -1,5 +1,11 @@
 import { useQuery } from "@tanstack/react-query"
+import { router } from "expo-router"
+import { useEffect } from "react"
 
+import { apiClient } from "@/src/lib/api-fetch"
+import { kv } from "@/src/lib/kv"
+
+import { isNewUserQueryKey, isOnboardingFinishedStorageKey } from "./constants"
 import { userSyncService, useUserStore } from "./store"
 
 export const whoamiQueryKey = ["user", "whoami"]
@@ -17,4 +23,27 @@ export const useWhoami = () => {
 
 export const useUser = (userId?: string) => {
   return useUserStore((state) => (userId ? state.users[userId] : undefined))
+}
+
+export function useIsNewUser() {
+  const { data } = useQuery({
+    queryKey: isNewUserQueryKey,
+    queryFn: async () => {
+      const subscriptions = await apiClient.subscriptions.$get({ query: {} })
+      const isOnboardingFinished = await kv.get(isOnboardingFinishedStorageKey)
+
+      return subscriptions.data.length < 5 && !isOnboardingFinished
+    },
+  })
+  return !!data
+}
+
+export function useOnboarding() {
+  const isNewUser = useIsNewUser()
+  useEffect(() => {
+    if (isNewUser) {
+      // @ts-expect-error
+      router.push("/onboarding")
+    }
+  }, [isNewUser])
 }
